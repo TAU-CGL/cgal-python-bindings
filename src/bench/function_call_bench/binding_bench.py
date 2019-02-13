ad954fb8a926be2ba94b2086748a633973d7e64a
Nir Goren <nirgoren@mail.tau.ac.il>
2019-02-13 16:01:42 +0200

    Added a benchmark comparing performance of bindings using shared_ptr Moved the function call benchmark to its own directory

--- src/bench/function_call_bench/binding_bench.py
+++ src/bench/function_call_bench/binding_bench.py
@@ -1,5 +1,6 @@
 import square
 import square_swig
+import square_boost
 import time
 import math
 
@@ -15,6 +16,13 @@ for i in range(j, times):
 t2 = time.process_time()
 print(t2-t1)
 
+print(small + "boost python")
+t1 = time.process_time()
+for i in range(j, times):
+    square_boost.square(i)
+t2 = time.process_time()
+print(t2-t1)
+
 print(small + "pybind11")
 t1 = time.process_time()
 for i in range(j, times):
@@ -22,13 +30,12 @@ for i in range(j, times):
 t2 = time.process_time()
 print(t2-t1)
 
-print(small + "python")
+print(small + "native python")
 t1 = time.process_time()
 for i in range(j, times):
     math.sqrt(i)
 t2 = time.process_time()
 print(t2-t1)
-
 
 
 big = "one big task: "
@@ -39,6 +46,11 @@ square_swig.squareTimes(j, times)
 t2 = time.process_time()
 print(t2-t1)
 
+print(big + "boost python")
+t1 = time.process_time()
+square_boost.squareTimes(j, times)
+t2 = time.process_time()
+print(t2-t1)
 
 print(big + "pybind11")
 t1 = time.process_time()
@@ -46,8 +58,7 @@ square.squareTimes(j, times)
 t2 = time.process_time()
 print(t2-t1)
 
-
-print(big + "python")
+print(big + "native python")
 t1 = time.process_time()
 res = 0
 for i in range(0, times):
