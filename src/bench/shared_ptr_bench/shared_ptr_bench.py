20191dbd7d3b9f58903ab03d0600886e354424c6
Nir Goren <nirgoren@mail.tau.ac.il>
2019-02-14 09:20:37 +0200

    Added boost-python to the shared_ptr benchmark Added results file

--- src/bench/shared_ptr_bench/shared_ptr_bench.py
+++ src/bench/shared_ptr_bench/shared_ptr_bench.py
@@ -1,5 +1,6 @@
 import sp_pybind11
 import swig_sp
+import boost_sp
 import time
 
 
@@ -21,6 +22,26 @@ t1 = time.process_time()
 a = sp_pybind11.MyClass()
 for i in range(j, times):
     lst[i] = sp_pybind11.getAnotherReference(a)
+    lst[i].set(i)
+t2 = time.process_time()
+print(t2-t1)
+
+
+print("many references to many objects - boost")
+t1 = time.process_time()
+for i in range(j, times):
+    a = boost_sp.MyClass()
+    a.set(i)
+t2 = time.process_time()
+print(t2-t1)
+
+
+lst = [0 for i in range(times)]
+print("many references to a single object - boost")
+t1 = time.process_time()
+a = boost_sp.MyClass()
+for i in range(j, times):
+    lst[i] = boost_sp.getAnotherReference(a)
     lst[i].set(i)
 t2 = time.process_time()
 print(t2-t1)
