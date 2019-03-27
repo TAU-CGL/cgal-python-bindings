d5b92c5e683e065531a362674069d31ad466f39c
Nir Goren <nirgoren@mail.tau.ac.il>
2019-03-27 13:44:10 +0200

    updated Polygon_2 python bindings

--- src/binding_examples_boost/polygon_2_boost/Polygon_2.cpp
+++ src/binding_examples_boost/polygon_2_boost/Polygon_2.cpp
@@ -4,14 +4,16 @@
 #include <CGAL/Polygon_2.h>
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 
-typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
-typedef CGAL::Point_2<K> Point_2;
-typedef CGAL::Polygon_2<K> Polygon_2;
+typedef int                                           Number_type;
+typedef CGAL::Simple_cartesian<Number_type>           Kernel;
+typedef CGAL::Point_2<Kernel> Point_2;
+typedef CGAL::Polygon_2<Kernel> Polygon_2;
+typedef CGAL::Bbox_2 Bbox_2;
 
 BOOST_PYTHON_MODULE(Polygon_2)
 {
 	using namespace boost::python;
-	class_<Point_2>("Point_2", init<double, double>())
+	class_<Point_2>("Point_2", init<int, int>())
 		//specifiying return policy is not needed in pybind11
 		.def("x", &Point_2::x, return_value_policy<copy_const_reference>())
 		.def("y", &Point_2::y, return_value_policy<copy_const_reference>())
@@ -21,5 +23,13 @@ BOOST_PYTHON_MODULE(Polygon_2)
 		.def("is_simple", &Polygon_2::is_simple)
 		.def("is_convex", &Polygon_2::is_convex)
 		.def("area", &Polygon_2::area)
+    .def("bbox", &Polygon_2::bbox)
 		;
+
+  class_<Bbox_2>("Bbox_2")
+    .def("xmin", &Bbox_2::xmin)
+    .def("ymin", &Bbox_2::ymin)
+    .def("xmax", &Bbox_2::xmax)
+    .def("ymax", &Bbox_2::ymax)
+    ;
 }
--- src/binding_examples_pybind11/pybind11_polygon_EPIEC/Polygon_2.cpp
+++ src/binding_examples_pybind11/pybind11_polygon_EPIEC/Polygon_2.cpp
@@ -12,28 +12,28 @@ typedef CGAL::Bbox_2 Bbox_2;
 
 PYBIND11_MODULE(Polygon_2, m)
 {
-	using namespace py;
-	class_<Point_2>(m, "Point_2")
-		.def(init<int, int>())
-		.def("x", &Point_2::x)
-		.def("y", &Point_2::y)
-		;
+using namespace py;
+class_<Point_2>(m, "Point_2")
+  .def(init<int, int>())
+  .def("x", &Point_2::x)
+  .def("y", &Point_2::y)
+  ;
 
-	class_<Polygon_2>(m, "Polygon_2")
-		.def(init<>())
-		.def("push_back", &Polygon_2::push_back)
-		.def("is_simple", &Polygon_2::is_simple)
-		.def("is_convex", &Polygon_2::is_convex)
-		.def("area", &Polygon_2::area)
-		.def("bbox", &Polygon_2::bbox)
-		;
+class_<Polygon_2>(m, "Polygon_2")
+  .def(init<>())
+  .def("push_back", &Polygon_2::push_back)
+  .def("is_simple", &Polygon_2::is_simple)
+  .def("is_convex", &Polygon_2::is_convex)
+  .def("area", &Polygon_2::area)
+  .def("bbox", &Polygon_2::bbox)
+  ;
 
-	class_<Bbox_2>(m, "Bbox_2")
-		.def("xmin", &Bbox_2::xmin)
-		.def("ymin", &Bbox_2::ymin)
-		.def("xmax", &Bbox_2::xmax)
-		.def("ymax", &Bbox_2::ymax)
-		;
+class_<Bbox_2>(m, "Bbox_2")
+  .def("xmin", &Bbox_2::xmin)
+  .def("ymin", &Bbox_2::ymin)
+  .def("xmax", &Bbox_2::xmax)
+  .def("ymax", &Bbox_2::ymax)
+  ;
 
 
 }
