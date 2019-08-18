0693fd0c772b8753f6f7c2e1a276ae6f1ef39849
Nir Goren <nirgoren@mail.tau.ac.il>
2019-08-18 16:45:02 +0200

    added bindings for shared_vector

--- src/libs/shared_vector/lib/shared_vector.cpp
+++ src/libs/shared_vector/lib/shared_vector.cpp
@@ -32,6 +32,13 @@ typedef boost::shared_ptr<Element_vector>       Shared_element_vector;
 //   void add_data(Shared_element element) { m_data->push_back(element); }
 // };
 
+Shared_element init_element()
+{
+  auto e = Shared_element(new Element);
+  e->set_str("new element");
+  return e;
+}
+
 Shared_element_vector initd()
 {
   auto data = Shared_element_vector(new Element_vector);
@@ -47,11 +54,33 @@ Shared_element_vector initd()
   return data;
 }
 
+Shared_element ev_get(Element_vector& ev, int i)
+{
+  return ev[i];
+}
+
+void ev_set(Element_vector& ev, int i, Shared_element& e)
+{
+  ev[i] = e;
+}
+
+Element_vector::iterator ev_begin(Element_vector& ev)
+{
+  return ev.begin();
+}
+
+Element_vector::iterator ev_end(Element_vector& ev)
+{
+  return ev.end();
+}
+
+
 BOOST_PYTHON_MODULE(shared_vector)
 {
   using namespace boost::python;
 
-  bp::register_ptr_to_python<Shared_element_vector>();
+  //bp::register_ptr_to_python<Shared_element_vector>();
+  //bp::register_ptr_to_python<Shared_element>();
 
   bp::class_<Element, Shared_element, boost::noncopyable>("Element", bp::no_init)
     .def("str", &Element::str,
@@ -59,5 +88,22 @@ BOOST_PYTHON_MODULE(shared_vector)
     .def("set_str", &Element::set_str);
     ;
 
+  bp::class_<Element_vector, Shared_element_vector>("Element_vector", bp::no_init)
+    // "ev[i]"
+    .def<Shared_element& (Element_vector::*)(const Element_vector::size_type)>
+    ("__getitem__", &Element_vector::operator[], return_value_policy<copy_non_const_reference>())
+    // less clustered alternative
+    //.def("__getitem__", &ev_get)
+
+    // "ev[i] = ..."
+    .def("__setitem__", &ev_set)
+
+    // "for element in ev.element():"
+    .def("elements", bp::range<return_value_policy<return_by_value>>(&ev_begin, &ev_end))
+  ;
+
+  
+
   bp::def("initd", initd);
+  bp::def("init_element", init_element);
 }
