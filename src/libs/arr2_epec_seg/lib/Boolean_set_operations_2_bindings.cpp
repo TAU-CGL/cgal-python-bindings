27ab4d11b6d7274c692fb5f74cf8c8a4fa84192b
Nir Goren <nirgoren@mail.tau.ac.il>
2019-09-10 23:07:23 +0300

    added do_intersect bindings

--- src/libs/arr2_epec_seg/lib/Boolean_set_operations_2_bindings.cpp
+++ src/libs/arr2_epec_seg/lib/Boolean_set_operations_2_bindings.cpp
@@ -16,10 +16,20 @@ void complement1(Polygon_with_holes_2& pgn, boost::python::list& lst)
   }
 }
 
+template <typename T0, typename T1>
+bool do_intersect(T0& P0, T1& P1)
+{
+  return CGAL::do_intersect(P0, P1);
+}
+
 
 void export_Boolean_set_operations_2()
 {
   using namespace boost::python;
   def("complement", complement0);
   def("complement", complement1);
+  def("do_intersect", &do_intersect<Polygon_2, Polygon_2>);
+  def("do_intersect", &do_intersect<Polygon_2, Polygon_with_holes_2>);
+  def("do_intersect", &do_intersect<Polygon_with_holes_2, Polygon_2>);
+  def("do_intersect", &do_intersect<Polygon_with_holes_2, Polygon_with_holes_2>);
 }
