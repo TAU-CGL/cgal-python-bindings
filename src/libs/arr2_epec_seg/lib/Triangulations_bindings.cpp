f286097a853874c8de0db2fa1cabb4dd0c729389
Nir Goren <nirgoren@mail.tau.ac.il>
2019-11-09 15:50:06 +0200

    Added x_at_y and y_at_x methods of Line_2 Fixed a bug with python overlay traits

--- src/libs/arr2_epec_seg/lib/Triangulations_bindings.cpp
+++ src/libs/arr2_epec_seg/lib/Triangulations_bindings.cpp
@@ -1,3 +1,11 @@
+// Copyright (c) 2019 Israel.
+// All rights reserved to Tel Aviv University.
+//
+// This file is private property of Tel Aviv University.
+//
+// Author(s): Nir Goren         <nirgoren@mail.tau.ac.il>
+//            Efi Fogel         <efifogel@gmail.com>
+
 #include "common.hpp"
 #if CGALPY_GEOMETRY_TRAITS == CGALPY_ARR_SEGMENT_TRAITS
 #include <CGAL/Triangulation_2.h>
