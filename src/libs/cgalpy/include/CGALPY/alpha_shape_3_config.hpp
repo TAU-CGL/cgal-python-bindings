37c6f0f189c8a784383667cd8c6e624ced21c7d1
nirgoren <nirgoren@mail.tau.ac.il>
2020-07-08 14:15:30 +0300

    Add alpha_shape_3 config and types file skeletons

---
+++ src/libs/cgalpy/include/CGALPY/alpha_shape_3_config.hpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2019 Israel.
+// All rights reserved to Tel Aviv University.
+//
+// This file is private property of Tel Aviv University.
+//
+// Author(s): Nir Goren         <nirgoren@mail.tau.ac.il>
+//            Efi Fogel         <efifogel@gmail.com>
+
+#ifndef CGALPY_ALPHA_SHAPE_3_CONFIG_HPP
+#define CGALPY_ALPHA_SHAPE_3_CONFIG_HPP
+
+#endif //CGALPY_ALPHA_SHAPE_3_CONFIG_HPP
