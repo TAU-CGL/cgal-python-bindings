a3a9fa4451be5493361921709711b8aeb1b8df06
Efi Fogel <efifogel@gmail.com>
2020-07-10 00:13:01 +0300

    Updated

--- src/python_scripts/arr2.py
+++ src/python_scripts/arr2.py
@@ -19,12 +19,15 @@ else:
 # now drag them in
 globals().update({k: getattr(mdl, k) for k in names})
 
+Arrangement_2 = ARR2.Arrangement_2
+Point_2 = KER.Point_2
+
 arr = Arrangement_2()
 c1 = Curve_2(Point_2(0, 0), Point_2(2, 0))
 c2 = Curve_2(Point_2(1, 2), Point_2(1, -2))
 c3 = Curve_2(Point_2(0, 0), Point_2(1, 2))
 c4 = Curve_2(Point_2(1, -2), Point_2(2, 0))
-insert(arr, [c1, c2, c3, c4])
+ARR2.insert(arr, [c1, c2, c3, c4])
 print("Number of faces in the arrangement:", arr.number_of_faces())
 print("Number of halfedges in the arrangement:", arr.number_of_halfedges())
 print("Number of vertices in the arrangement:", arr.number_of_vertices())
