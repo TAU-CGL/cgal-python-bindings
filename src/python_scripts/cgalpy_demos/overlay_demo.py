b08f229db4833757f0f5ef390916d987220ebf23
Nir Goren <nirgoren@mail.tau.ac.il>
2021-04-29 10:06:05 +0300

    Add comments to demos

--- src/python_scripts/cgalpy_demos/overlay_demo.py
+++ src/python_scripts/cgalpy_demos/overlay_demo.py
@@ -25,16 +25,12 @@ Curve_2 = Arr2.Curve_2
 from gui_demo import GUI_demo, QtWidgets, QtCore
 
 
-def plus(x, y):
-    return x + y
-
-
 def set_up_arrangement():
     arr0 = Arrangement_2()
-    p0 = Point_2(0, 1)
-    p1 = Point_2(1, 2)
-    p2 = Point_2(2, 1)
-    p3 = Point_2(1, 0)
+    p0 = Point_2(0, 100)
+    p1 = Point_2(100, 200)
+    p2 = Point_2(200, 100)
+    p3 = Point_2(100, 0)
     c0 = Curve_2(Segment_2(p0, p1))
     c1 = Curve_2(Segment_2(p1, p2))
     c2 = Curve_2(Segment_2(p2, p3))
@@ -42,16 +38,25 @@ def set_up_arrangement():
 
     Arr2.insert(arr0, [c0, c1, c2, c3])
 
-    for face in arr0.faces():
-        face.set_data(0)
-        if not face.is_unbounded():
-            face.set_data(1)
+# Set the data for the faces. The data can be any python object
+    for f in arr0.faces():
+        if f.is_unbounded():
+            f.set_data(0)
+        else:
+            f.set_data(1)
+
+    # Alternative
+    # ubf: Face = arr0.unbounded_face()
+    # ubf.set_data(0)
+    # f: Face
+    # for ccb in ubf.inner_ccbs():
+    #     next(next(ubf.inner_ccbs())).twin().face().set_data(1)
 
     arr1 = Arrangement_2()
-    p0 = Point_2(0.5, 1)
-    p1 = Point_2(1.5, 2)
-    p2 = Point_2(2.5, 1)
-    p3 = Point_2(1.5, 0)
+    p0 = Point_2(50, 100)
+    p1 = Point_2(150, 200)
+    p2 = Point_2(250, 100)
+    p3 = Point_2(150, 0)
     c0 = Curve_2(Segment_2(p0, p1))
     c1 = Curve_2(Segment_2(p1, p2))
     c2 = Curve_2(Segment_2(p2, p3))
@@ -59,26 +64,29 @@ def set_up_arrangement():
 
     Arr2.insert(arr1, [c0, c1, c2, c3])
 
-    for face in arr1.faces():
-        face.set_data(0)
-        if not face.is_unbounded():
-            face.set_data(2)
+    for f in arr1.faces():
+        if f.is_unbounded():
+            f.set_data(0)
+        else:
+            f.set_data(1)
 
-    traits = Arr2.Arr_face_overlay_traits(plus)  # Define how a face data will be determined (sum of the data of the
-    # two overlaid faces)
-    arr2 = Arrangement_2()
-    Arr2.overlay(arr0, arr1, arr2, traits)
+    result = Arrangement_2()
+    # Set the data of a face that is the intersection of two faces
+    # from the original arrangements to be the sum of their data
+    overlay_traits = Arr2.Arr_face_overlay_traits(lambda x, y: x+y)
+    Arr2.overlay(arr0, arr1, result, overlay_traits)
 
-    # splitting a preexisting face
-    p1 = Point_2(2, 1)
-    p2 = Point_2(2.5, 1)
+    # Splitting a preexisting face setting the data of the two new faces to match the old face data
+    p1 = Point_2(200, 100)
+    p2 = Point_2(250, 100)
     c = Curve_2(Segment_2(p1, p2))
-    he = Arr2.insert_non_intersecting_curve(arr2, c)
+    he = Arr2.insert_non_intersecting_curve(result, c)
     face_data = he.face().data() if he.face().data() is not None else he.twin().face().data()
     he.face().set_data(face_data)
     he.twin().face().set_data(face_data)
 
-    for face in arr2.faces():
+    # Drawing
+    for face in result.faces():
         print(face.data())
         lst = []
         if not face.is_unbounded():
