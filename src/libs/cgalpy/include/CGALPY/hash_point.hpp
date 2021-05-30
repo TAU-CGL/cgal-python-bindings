6f004f5c85c8b83c916df4c22caa7a5e2ae32932
Efi Fogel <efifogel@gmail.com>
2021-05-30 20:06:28 +0300

    Updated

--- src/libs/cgalpy/include/CGALPY/hash_point.hpp
+++ src/libs/cgalpy/include/CGALPY/hash_point.hpp
@@ -11,18 +11,22 @@
 
 #include <boost/functional/hash_fwd.hpp>
 
+#include <CGAL/basic.h>
+#include <CGAL/Gmpq.h>
+#include <CGAL/Algebraic_structure_traits.h>
+
 #if CGALPY_KERNEL == CGALPY_KERNEL_EPEC
 template <typename Point>
 size_t hash_rational_point(Point& p)
 {
-  auto simplify = CGAL::Algebraic_structure_traits<Gmpq>::Simplify();
+  auto simplify = CGAL::Algebraic_structure_traits<CGAL::Gmpq>::Simplify();
   size_t seed = 0;
   for (auto c = p.cartesian_begin(); c != p.cartesian_end(); ++c)
   {
     auto q = (*c).exact();
-    simplify(q);
-    boost::hash_combine(seed, q.numerator().to_double());
-    boost::hash_combine(seed, q.denominator().to_double());
+    // simplify(q);
+    // boost::hash_combine(seed, q.numerator().to_double());
+    // boost::hash_combine(seed, q.denominator().to_double());
   }
   return seed;
 }
