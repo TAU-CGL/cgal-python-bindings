d4512c072849987eff195d7fa6d570ca86b7f405
Efi Fogel <efifogel@gmail.com>
2021-05-31 17:35:36 +0300

    Fixed generic hash function for point in epec kernel

--- src/libs/cgalpy/include/CGALPY/kernel_config.hpp
+++ src/libs/cgalpy/include/CGALPY/kernel_config.hpp
@@ -13,7 +13,8 @@
 
 // Prevent compilation issues with some kernels
 #define CGAL_DO_NOT_USE_BOOST_MP 1
-#undef CGAL_USE_GMPXX
+// Suppressing GMPXX is not recommended, but if you need to, uncomment the next
+// #undef CGAL_USE_GMPXX
 
 #define CGALPY_KERNEL_EPEC 0
 #define CGALPY_KERNEL_EPIC 1
