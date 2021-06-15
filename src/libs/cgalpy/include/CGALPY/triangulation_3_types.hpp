2236a02a2821425f35869dd76c21154eba08e411
Efi Fogel <efifogel@gmail.com>
2021-06-15 17:13:36 +0300

    Updated

--- src/libs/cgalpy/include/CGALPY/triangulation_3_types.hpp
+++ src/libs/cgalpy/include/CGALPY/triangulation_3_types.hpp
@@ -59,7 +59,11 @@
 // 3D triangulation traits
 #if CGALPY_TRI3_TRAITS == CGALPY_TRI3_TRAITS_KERNEL
 // Nothing to include
-#elif CGALPY_TRI3_TRAITS == CGALPY_TRI3_TRAITS_PERIODIC3_DELAUNAY
+#elif CGALPY_TRI3_TRAITS == CGALPY_TRI3_TRAITS_PERIODIC_PLAIN
+#include <CGAL/Periodic_3_triangulation_3.h>
+#elif CGALPY_TRI3_TRAITS == CGALPY_TRI3_TRAITS_PERIODIC_REGULAR
+#include <CGAL/Periodic_3_regular_triangulation_traits_3.h>
+#elif CGALPY_TRI3_TRAITS == CGALPY_TRI3_TRAITS_PERIODIC_DELAUNAY
 #include <CGAL/Periodic_3_Delaunay_triangulation_traits_3.h>
 #else
 BOOST_STATIC_ASSERT_MSG(false, "CGALPY_TRI3_TRAITS");
@@ -72,8 +76,12 @@ BOOST_STATIC_ASSERT_MSG(false, "CGALPY_TRI3_TRAITS");
 #include <CGAL/Regular_triangulation_3.h>
 #elif CGALPY_TRI3 == CGALPY_TRI3_DELAUNAY
 #include <CGAL/Delaunay_triangulation_3.h>
-#elif CGALPY_TRI3 == CGALPY_TRI3_PERIODIC3_DELAUNAY
-#include <CGAL/eriodic_3_Delaunay_triangulation_3.h>
+#elif CGALPY_TRI3 == CGALPY_TRI3_PERIODIC_PLAIN
+#include <CGAL/Periodic_3_triangulation_3.h>
+#elif CGALPY_TRI3 == CGALPY_TRI3_PERIODIC_REGULAR
+#include <CGAL/Periodic_3_regular_triangulation_3.h>
+#elif CGALPY_TRI3 == CGALPY_TRI3_PERIODIC_DELAUNAY
+#include <CGAL/Periodic_3_Delaunay_triangulation_3.h>
 #else
 BOOST_STATIC_ASSERT_MSG(false, "CGALPY_TRI3");
 #endif
@@ -92,23 +100,23 @@ typedef CGAL::Regular_triangulation_vertex_base_3<Kernel>       Vb0;
 typedef CGAL::Triangulation_vertex_base_with_info_3<size_t, Kernel, Vb0> Vertex_base;
 #elif CGALPY_TRI3_VERTEX_BASE == CGALPY_TRI3_VERTEX_BASE_ALPHA_SHAPE
 typedef CGAL::Triangulation_vertex_base_3<Kernel>               Vb0;
-  typedef CGAL::Alpha_shape_vertex_base_3<Kernel, Vb0, as3::Exact_comparison> Vertex_base;
+typedef CGAL::Alpha_shape_vertex_base_3<Kernel, Vb0, as3::Exact_comparison> Vertex_base;
 #elif CGALPY_TRI3_VERTEX_BASE == CGALPY_TRI3_VERTEX_BASE_ALPHA_SHAPE_WITH_INFO
 typedef CGAL::Triangulation_vertex_base_with_info_3<size_t, Kernel> Vb0;
-  typedef CGAL::Alpha_shape_vertex_base_3<Kernel, Vb0, as3::Exact_comparison> Vertex_base;
+typedef CGAL::Alpha_shape_vertex_base_3<Kernel, Vb0, as3::Exact_comparison> Vertex_base;
 #elif CGALPY_TRI3_VERTEX_BASE == CGALPY_TRI3_VERTEX_BASE_ALPHA_SHAPE_REGULAR
 typedef CGAL::Regular_triangulation_vertex_base_3<Kernel>       Vb0;
-  typedef CGAL::Alpha_shape_vertex_base_3<Kernel, Vb0, as3::Exact_comparison> Vertex_base;
+typedef CGAL::Alpha_shape_vertex_base_3<Kernel, Vb0, as3::Exact_comparison> Vertex_base;
 #elif CGALPY_TRI3_VERTEX_BASE == CGALPY_TRI3_VERTEX_BASE_ALPHA_SHAPE_REGULAR_WITH_INFO
 typedef CGAL::Regular_triangulation_vertex_base_3<Kernel>       Vb0;
 typedef CGAL::Triangulation_vertex_base_with_info_3<size_t, Kernel, Vb0> Vb1;
 typedef CGAL::Fixed_alpha_shape_vertex_base_3<Kernel, Vb1>      Vertex_base;
 #elif CGALPY_TRI3_VERTEX_BASE == CGALPY_TRI3_VERTEX_BASE_FIXED_ALPHA_SHAPE
 typedef CGAL::Triangulation_vertex_base_3<Kernel>               Vb0;
-  typedef CGAL::Alpha_shape_vertex_base_3<Kernel, Vb0, as3::Exact_comparison> Vertex_base;
+typedef CGAL::Fixed_alpha_shape_vertex_base_3<Kernel, Vb0>      Vertex_base;
 #elif CGALPY_TRI3_VERTEX_BASE == CGALPY_TRI3_VERTEX_BASE_FIXED_ALPHA_SHAPE_WITH_INFO
 typedef CGAL::Triangulation_vertex_base_with_info_3<size_t, Kernel> Vb0;
-typedef CGAL::Fixed_alpha_shape_vertex_base_3<Kernel, Vb0>     Vertex_base;
+typedef CGAL::Fixed_alpha_shape_vertex_base_3<Kernel, Vb0>      Vertex_base;
 #elif CGALPY_TRI3_VERTEX_BASE == CGALPY_TRI3_VERTEX_BASE_FIXED_ALPHA_SHAPE_REGULAR
 typedef CGAL::Regular_triangulation_vertex_base_3<Kernel>       Vb0;
 typedef CGAL::Fixed_alpha_shape_vertex_base_3<Kernel, Vb0>      Vertex_base;
@@ -145,7 +153,7 @@ typedef CGAL::Triangulation_cell_base_with_info_3<size_t, Kernel, Cb0> Cb1;
 typedef CGAL::Fixed_alpha_shape_cell_base_3<Kernel, Cb1>        Cell_base;
 #elif CGALPY_TRI3_CELL_BASE == CGALPY_TRI3_CELL_BASE_FIXED_ALPHA_SHAPE
 typedef CGAL::Triangulation_cell_base_3<Kernel>                 Cb0;
-typedef CGAL::Alpha_shape_cell_base_3<Kernel, Cb0, as3::Exact_comparison> Cell_base;
+typedef CGAL::Fiexed_alpha_shape_cell_base_3<Kernel, Cb0>       Cell_base;
 #elif CGALPY_TRI3_CELL_BASE == CGALPY_TRI3_CELL_BASE_FIXED_ALPHA_SHAPE_WITH_INFO
 typedef CGAL::Triangulation_cell_base_with_info_3<size_t, Kernel> Cb0;
 typedef CGAL::Fixed_alpha_shape_cell_base_3<Kernel, Cb0>        Cell_base;
@@ -161,9 +169,9 @@ BOOST_STATIC_ASSERT_MSG(false, "CGALPY_TRI3_CELL_BASE");
 #endif
 
 // 3D triangulation concurrency
-#if CGALPY_TRI3_CONCURRENCY == CGALPY_TRI3_TRAITS_SEQUENTIAL
+#if CGALPY_TRI3_CONCURRENCY == CGALPY_TRI3_CONCURRENCY_SEQUENTIAL
 typedef CGAL::Sequential_tag                                       Concurrency_tag;
-#elif CGALPY_TRI3_CONCURRENCY == CGALPY_TRI3_TRAITS_PARALLEL
+#elif CGALPY_TRI3_CONCURRENCY == CGALPY_TRI3_CONCURRENCY_PARALLEL
 typedef CGAL::Parallel_tag                                         Concurrency_tag;
 #else
 BOOST_STATIC_ASSERT_MSG(false, "CGALPY_TRI3_CONCURRENCY");
@@ -174,7 +182,11 @@ typedef CGAL::Triangulation_data_structure_3<Vertex_base, Cell_base, Concurrency
 // 3D triangulation traits
 #if CGALPY_TRI3_TRAITS == CGALPY_TRI3_TRAITS_KERNEL
 typedef Kernel                                                     Traits;
-#elif CGALPY_TRI3_TRAITS == CGALPY_TRI3_TRAITS_PERIODIC3_DELAUNAY
+#elif CGALPY_TRI3_TRAITS == CGALPY_TRI3_TRAITS_PERIODIC_PLAIN
+typedef CGAL::Periodic_3_triangulation_traits_3<Kernel>            Traits;
+#elif CGALPY_TRI3_TRAITS == CGALPY_TRI3_TRAITS_PERIODIC_REGULAR
+typedef CGAL::Periodic_3_regular_triangulation_traits_3<Kernel>    Traits;
+#elif CGALPY_TRI3_TRAITS == CGALPY_TRI3_TRAITS_PERIODIC_DELAUNAY
 typedef CGAL::Periodic_3_Delaunay_triangulation_traits_3<Kernel>   Traits;
 #else
 BOOST_STATIC_ASSERT_MSG(false, "CGALPY_TRI3_TRAITS");
@@ -201,8 +213,15 @@ typedef Triangulation_3::Bare_point                                  Bare_point;
 #elif CGALPY_TRI3 == CGALPY_TRI3_DELAUNAY
 typedef CGAL::Delaunay_triangulation_3<Traits, Tds, Location_policy> Delaunay_triangulation_3;
 typedef Delaunay_triangulation_3                                     Triangulation_3;
-#elif CGALPY_TRI3 == CGALPY_TRI3_PERIODIC3_DELAUNAY
-typedef CGAL::Periodic_3_Delaunay_triangulation_3<Traits, Tds>       Triangulation_3;
+#elif CGALPY_TRI3 == CGALPY_TRI3_PERIODIC_PLAIN
+typedef CGAL::Periodic_3_triangulation_3<Traits, Tds>                Triangulation_3;
+#elif CGALPY_TRI3 == CGALPY_TRI3_PERIODIC_REGULAR
+typedef CGAL::Periodic_3_regular_triangulation_3<Traits, Tds>        Triangulation_3;
+typedef Triangulation_3::Weighted_point                              Weighted_point;
+typedef Triangulation_3::Bare_point                                  Bare_point;
+#elif CGALPY_TRI3 == CGALPY_TRI3_PERIODIC_DELAUNAY
+typedef CGAL::Periodic_3_Delaunay_triangulation_3<Traits, Tds>       Delaunay_triangulation_3;
+typedef Delaunay_triangulation_3                                     Triangulation_3;
 #else
 BOOST_STATIC_ASSERT_MSG(false, "CGALPY_TRI3");
 #endif
