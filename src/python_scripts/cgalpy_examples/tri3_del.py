b9a5815aa8b1df4ca00dccf53b172ccbedd548a6
Efi Fogel <efifogel@gmail.com>
2021-06-17 16:06:16 +0300

    Updated

--- src/python_scripts/cgalpy_examples/tri3_del.py
+++ src/python_scripts/cgalpy_examples/tri3_del.py
@@ -14,15 +14,12 @@ else:
 CGALPY = importlib.import_module(lib)
 Ker = CGALPY.Ker
 Tri3 = CGALPY.Tri3
-
 Point_3 = Ker.Point_3
-
 p1 = Point_3(0, 0, 0)
 p2 = Point_3(1, 0, 0)
 p3 = Point_3(0, 1, 0)
 p4 = Point_3(0, 0, 1)
 p5 = Point_3(2, 2, 2)
 p6 = Point_3(-1, 0, 1)
-
 dt = Tri3.Delaunay_triangulation_3([p1, p2, p3, p4, p5, p6])
 print(dt.is_valid(False, 0))
