d09bfbfd8b9fd3e4ffe38714cc611ca59aefd69d
Efi Fogel <efifogel@gmail.com>
2021-06-20 18:24:43 +0300

    Updated

--- src/python_scripts/cgalpy_examples/as3_del_compact_epic.py
+++ src/python_scripts/cgalpy_examples/as3_del_compact_epic.py
@@ -14,23 +14,18 @@ else:
 CGALPY = importlib.import_module(lib)
 Ker = CGALPY.Ker
 AS3 = CGALPY.AS3
-
 Point_3 = Ker.Point_3
 Alpha_shape_3 = AS3.Alpha_shape_3
 Mode = AS3.Mode
-
 p1 = Point_3(492, 291, 677)
 p2 = Point_3(493, 314, 533)
 p3 = Point_3(494, 326, 462)
 p4 = Point_3(493, 303, 605)
 alphaShape = Alpha_shape_3([p1, p2, p3, p4])
-
 print("Alpha shape computed in REGULARIZED mode by default")
-# Find alpha solid
-alphaSolid = alphaShape.find_alpha_solid()
+alphaSolid = alphaShape.find_alpha_solid()   # Find alpha solid
 print("Smallest alpha value to get a solid through data points is ", alphaSolid)
-# find optimal alpha value
-optHandle = alphaShape.find_optimal_alpha(1)
+optHandle = alphaShape.find_optimal_alpha(1) # find optimal alpha value
 optAlpha = next(optHandle)
 print("Optimal alpha value to get one connected component is ", optAlpha)
 alphaShape.set_alpha(optAlpha)
