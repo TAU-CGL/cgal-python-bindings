a7213ada612e1c484b888fbc5103cb7fa2c055c6
Efi Fogel <efifogel@gmail.com>
2021-06-29 18:13:52 +0300

    Cleaned up

--- src/libs/cgalpy/lib/arr_point_location_bindings.cpp
+++ src/libs/cgalpy/lib/arr_point_location_bindings.cpp
@@ -8,7 +8,13 @@
 
 #include <boost/python.hpp>
 
-#include "CGALPY/arr_point_location_types.hpp"
+#include <CGAL/Arr_naive_point_location.h>
+#include <CGAL/Arr_walk_along_line_point_location.h>
+#include <CGAL/Arr_trapezoid_ric_point_location.h>
+#include <CGAL/Arr_landmarks_point_location.h>
+
+#include "CGALPY/arr_point_location_config.hpp"
+#include "CGALPY/arrangement_on_surface_2_types.hpp"
 
 namespace bp = boost::python;
 
@@ -46,6 +52,7 @@ bool get_type(Pl_result& pl_result, typename type::value_type& t) {
   if (res) t = *(*get);
   return res;
 }
+
 #if CGALPY_AOS2_GEOMETRY_TRAITS == CGALPY_AOS2_LINEAR_GEOMETRY_TRAITS || \
   CGALPY_AOS2_GEOMETRY_TRAITS == CGALPY_AOS2_SEGMENT_GEOMETRY_TRAITS || \
   CGALPY_AOS2_GEOMETRY_TRAITS == CGALPY_AOS2_NON_CACHING_SEGMENT_GEOMETRY_TRAITS
