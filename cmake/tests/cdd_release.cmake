0f1a0dac2ac27412abdd15e9fa5fb68795191c01
Efi Fogel <efifogel@gmail.com>
2021-06-30 21:21:44 +0300

    1st revision

---
+++ cmake/tests/cdd_release.cmake
@@ -0,0 +1,6 @@
+set(CMAKE_BUILD_TYPE "Release"               CACHE STRING "build type" FORCE)
+set(CGALPY_USE_SHARED_LIBS ON                CACHE BOOL "" FORCE)
+set(CGALPY_FIXED_LIBRARY_NAME OFF            CACHE BOOL "" FORCE)
+set(CGALPY_KERNEL_BINDINGS OFF               CACHE BOOL "Disable kernel" FORCE)
+set(CGALPY_KERNEL_D_BINDINGS ON              CACHE BOOL "Enable dD kernel" FORCE)
+set(CGALPY_KERNEL_D_NAME "cartesiandDouble"  CACHE STRING "use EPICD kernel" FORCE)
