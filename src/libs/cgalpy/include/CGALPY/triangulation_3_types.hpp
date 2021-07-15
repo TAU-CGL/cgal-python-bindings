a93e69a2a97ba01706fcf4d7acdef8d069d5ee93
Efi Fogel <efifogel@gmail.com>
2021-07-15 20:18:37 +0300

    enhances for 3d triangulations

--- src/libs/cgalpy/include/CGALPY/triangulation_3_types.hpp
+++ src/libs/cgalpy/include/CGALPY/triangulation_3_types.hpp
@@ -47,28 +47,43 @@ typedef Location_policy<CGALPY_TRI3_LOCATION_POLICY>::type      Lp;
 typedef Base_tri<CGALPY_TRI3, Traits, Tds, Lp>::type            Btr;
 typedef Tri<hierarchy(), Btr>::type                             Triangulation_3;
 
+typedef Triangulation_3::Geom_traits                  Geom_traits;
+typedef Triangulation_3::Triangulation_data_structure
+  Triangulation_data_structure;
+typedef Triangulation_3::Lock_data_structure          Lock_data_structure;
+typedef Triangulation_3::Locate_type                  Locate_type;
+
 typedef Triangulation_3::Point                        Point;
 typedef Triangulation_3::Segment                      Segment;
 typedef Triangulation_3::Triangle                     Triangle;
+typedef Triangulation_3::Tetrahedron                  Tetrahedron;
 typedef Triangulation_3::Vertex                       Vertex;
 typedef Triangulation_3::Edge                         Edge;
 typedef Triangulation_3::Facet                        Facet;
 typedef Triangulation_3::Cell                         Cell;
 typedef Triangulation_3::size_type                    size_type;
 typedef Triangulation_3::difference_type              difference_type;
+typedef Triangulation_3::Concurrency_tag              Concurrency_tag;
 
 typedef Triangulation_3::Vertex_handle                Vertex_handle;
 typedef Triangulation_3::Cell_handle                  Cell_handle;
+typedef Triangulation_3::Simplex                      Simplex;
 
 typedef Triangulation_3::All_vertices_iterator        All_vertices_iterator;
-typedef Triangulation_3::Finite_vertices_iterator     Finite_vertices_iterator;
 typedef Triangulation_3::All_edges_iterator           All_edges_iterator;
-typedef Triangulation_3::Finite_edges_iterator        Finite_edges_iterator;
-typedef Triangulation_3::Cell_handle                  Cell_handle;
+typedef Triangulation_3::All_facets_iterator          All_facets_iterator;
 typedef Triangulation_3::All_cells_iterator           All_cells_iterator;
+
+typedef Triangulation_3::Finite_vertices_iterator     Finite_vertices_iterator;
+typedef Triangulation_3::Finite_edges_iterator        Finite_edges_iterator;
+typedef Triangulation_3::Finite_facets_iterator       Finite_facets_iterator;
 typedef Triangulation_3::Finite_cells_iterator        Finite_cells_iterator;
+
 typedef Triangulation_3::Point_iterator               Point_iterator;
-typedef Triangulation_3::Locate_type                  Locate_type;
+typedef Triangulation_3::Facet_iterator               Facet_iterator;
+typedef Triangulation_3::Cell_iterator                Cell_iterator;
+typedef Triangulation_3::Segment_cell_iterator        Segment_cell_iterator;
+typedef Triangulation_3::Segment_simplex_iterator     Segment_simplex_iterator;
 
 #if ((CGALPY_TRI3 == CGALPY_TRI3_PLAIN) || \
      (CGALPY_TRI3 == CGALPY_TRI3_PERIODIC_PLAIN))
