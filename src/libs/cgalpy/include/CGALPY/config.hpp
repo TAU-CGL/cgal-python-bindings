3a47c7fa7307061267e94c71a6e1287b58489f88
Nir Goren <nirgoren@mail.tau.ac.il>
2021-07-19 10:07:37 +0300

    MSVC fix for DETECT_EXIST macro

--- src/libs/cgalpy/include/CGALPY/config.hpp
+++ src/libs/cgalpy/include/CGALPY/config.hpp
@@ -9,6 +9,8 @@
 #ifndef CGALPY_CONFIG_HPP
 #define CGALPY_CONFIG_HPP
 
+#define EXPAND( x ) x
+
 #define STR(s) #s
 #define XSTR(s) STR(s)
 
@@ -21,7 +23,7 @@
 
 // DETECT_EXIST merely concats a converted macro to the end of DETECT_EXIST_TRUE.
 // If empty, DETECT_EXIST_TRUE converts fine.  If not 0 remains second argument.
-#define DETECT_EXIST_IMPL(...) SECOND_ARG(__VA_ARGS__)
+#define DETECT_EXIST_IMPL(...) EXPAND(SECOND_ARG(__VA_ARGS__))
 #define DETECT_EXIST(X) DETECT_EXIST_IMPL(CONCAT2(DETECT_EXIST_TRUE,X), 0, ~)
 
 #endif //CONFIG_HPP
