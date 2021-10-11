62c4e65bd67f148ed77d398c6abaf11274072aa2
Efi Fogel <efifogel@gmail.com>
2021-10-11 19:37:46 +0300

    Fixed definition of the traits class of the arrangement when bindingss for regularized boolean set operation  are generated

--- src/libs/cgalpy/include/CGALPY/arrangement_on_surface_2_config.hpp
+++ src/libs/cgalpy/include/CGALPY/arrangement_on_surface_2_config.hpp
@@ -10,7 +10,9 @@
 #define CGALPY_ARRANGEMENT_ON_SURFACE_2_CONFIG_HPP
 
 #include <CGAL/Arr_extended_dcel.h>
+#include <CGAL/Gps_traits_2.h>
 #include <CGAL/Boolean_set_operations_2/Gps_default_dcel.h>
+
 #include "config.hpp"
 
 #define CGALPY_AOS2_LINEAR_GEOMETRY_TRAITS              0
@@ -39,6 +41,12 @@ constexpr bool is_face_extended()
 { return DETECT_EXIST(CGALPY_AOS2_FACE_EXTENDED); }
 
 // Boolean set operation
+
+template <bool b, typename BaseTr> struct Tr {};
+template <typename Base>
+struct Tr<false, Base>{ typedef Base type; };
+template <typename Base>
+struct Tr<true, Base>{ typedef CGAL::Gps_traits_2<Base> type; };
 
 template <bool b, typename Tr> struct Halfedge_gps {};
 template <typename Tr>
