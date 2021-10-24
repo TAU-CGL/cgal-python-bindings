63c3d8dbf51ad692f95f4c906cf388148e864f2d
Efi Fogel <efifogel@gmail.com>
2021-10-25 00:57:36 +0300

    Cleaned up

--- src/libs/cgalpy/include/CGALPY/aos_2_concepts/export_AosBasicTraits.hpp
+++ src/libs/cgalpy/include/CGALPY/aos_2_concepts/export_AosBasicTraits.hpp
@@ -14,12 +14,14 @@
 // Introduce a new class object
 // Observe that bp::no_init is passed to the constructor of the class object, which makes this
 // template function unsuitable for Point_2 and X_montone_curve_2.
-template <typename Type, const char* Name>
-bp::class_<Type>* add_class_object(bp::scope& my_scope) {
+template <typename Type, const char* Name, typename RVP = bp::default_call_policies>
+void add_class_object(bp::scope& my_scope, bp::class_<Type>*& co) {
   bp::handle<> tco(bp::objects::registered_class_object(bp::type_id<Type>()));
-  if (tco.get() == 0) return new bp::class_<Type>(Name, bp::no_init);
-  my_scope.attr(Name) = tco;
-  return nullptr;
+  if (tco.get() != 0) my_scope.attr(Name) = tco;
+  else {
+    co = new bp::class_<Type>(Name, bp::no_init);
+    co->def("__call__", &Type::operator(), RVP());
+  }
 }
 
 //
@@ -28,10 +30,8 @@ void export_compare_at_x_left_2(C c, Classes& classes, CGAL::Tag_true) {
   typedef typename T::Compare_y_at_x_left_2     Compare_y_at_x_left_2;
   bp::scope traits_scope(c);
   static const char compare_y_at_x_left_2[] = "Compare_y_at_x_left_2";
-  classes.m_compare_y_at_x_left_2 =
-    add_class_object<Compare_y_at_x_left_2, compare_y_at_x_left_2>(traits_scope);
-  if (classes.m_compare_y_at_x_left_2)
-    classes.m_compare_y_at_x_left_2->def("__call__", &T::Compare_y_at_x_left_2::operator());
+  add_class_object<Compare_y_at_x_left_2, compare_y_at_x_left_2>
+    (traits_scope, classes.m_compare_y_at_x_left_2);
 
   c.def("compare_y_at_x_left_2_object", &T::compare_y_at_x_left_2_object);
 }
@@ -81,52 +81,41 @@ void export_AosBasicTraits(C c, Concepts& concepts) {
 
   // Compare_x_2
   static const char compare_x_2[] = "Compare_x_2";
-  classes.m_compare_x_2 = add_class_object<Compare_x_2, compare_x_2>(traits_scope);
-  if (classes.m_compare_x_2) classes.m_compare_x_2->def("__call__", &Compare_x_2::operator());
+  add_class_object<Compare_x_2, compare_x_2>(traits_scope, classes.m_compare_x_2);
 
   // Compare_xy_2
   static const char compare_xy_2[] = "Compare_xy_2";
-  classes.m_compare_xy_2 = add_class_object<Compare_xy_2, compare_x_2>(traits_scope);
-  if (classes.m_compare_xy_2)
-    classes.m_compare_xy_2->def("__call__", &Compare_xy_2::operator());
+  add_class_object<Compare_xy_2, compare_x_2>(traits_scope, classes.m_compare_xy_2);
 
   // Construct_min_vertex_2
   static const char construct_min_vertex_2[] = "Construct_min_vertex_2";
-  classes.m_construct_min_vertex_2 =
-    add_class_object<Construct_min_vertex_2, construct_min_vertex_2>(traits_scope);
-  if (classes.m_construct_min_vertex_2)
-    classes.m_construct_min_vertex_2->def("__call__", &Construct_min_vertex_2::operator(), RVP());
+  add_class_object<Construct_min_vertex_2, construct_min_vertex_2, RVP>
+    (traits_scope, classes.m_construct_min_vertex_2);
 
   // Construct_max_vertex_2
   static const char construct_max_vertex_2[] = "Construct_max_vertex_2";
-  classes.m_construct_max_vertex_2 =
-    add_class_object<Construct_max_vertex_2, construct_max_vertex_2>(traits_scope);
-  classes.m_construct_max_vertex_2->def("__call__", &Construct_max_vertex_2::operator(), RVP());
+  add_class_object<Construct_max_vertex_2, construct_max_vertex_2, RVP>
+    (traits_scope, classes.m_construct_max_vertex_2);
 
   // Is_vertical_2
   static const char is_vertical_2[] = "Is_vertical_2";
-  classes.m_is_vertical_2 = add_class_object<Is_vertical_2, is_vertical_2>(traits_scope);
-  if (classes.m_is_vertical_2)
-    classes.m_is_vertical_2->def("__call__", &Is_vertical_2::operator());
+  add_class_object<Is_vertical_2, is_vertical_2>(traits_scope, classes.m_is_vertical_2);
 
   // Compare_y_at_x_2
   static const char compare_y_at_x_2[] = "Compare_y_at_x_2";
-  classes.m_compare_y_at_x_2 =
-    add_class_object<Compare_y_at_x_2, compare_y_at_x_2>(traits_scope);
-  if (classes.m_compare_y_at_x_2)
-    classes.m_compare_y_at_x_2->def("__call__", &Compare_y_at_x_2::operator());
+  add_class_object<Compare_y_at_x_2, compare_y_at_x_2>
+    (traits_scope, classes.m_compare_y_at_x_2);
 
   // Compare_y_at_x_right_2
   static const char compare_y_at_x_right_2[] = "Compare_y_at_x_right_2";
-  classes.m_compare_y_at_x_right_2 =
-    add_class_object<Compare_y_at_x_right_2, compare_y_at_x_right_2>(traits_scope);
-  if (classes.m_compare_y_at_x_right_2)
-    classes.m_compare_y_at_x_right_2->def("__call__", &Compare_y_at_x_right_2::operator());
+  add_class_object<Compare_y_at_x_right_2, compare_y_at_x_right_2>
+    (traits_scope, classes.m_compare_y_at_x_right_2);
 
   // Equal_2
-  static const char equal_2[] = "Equal_2";
-  classes.m_equal_2 = add_class_object<Equal_2, equal_2>(traits_scope);
-  if (classes.m_equal_2)  {
+  bp::handle<> eq_co(bp::objects::registered_class_object(bp::type_id<Equal_2>()));
+  if (eq_co.get() != 0) traits_scope.attr("Equal_2") = eq_co;
+  else {
+    classes.m_equal_2 = new bp::class_<Equal_2>("Equal_2", bp::no_init);
     using Equal1 = bool(Equal_2::*)(const Point_2&, const Point_2&) const;
     using Equal2 = bool(Equal_2::*)(const X_monotone_curve_2&, const X_monotone_curve_2&) const;
     classes.m_equal_2->def("__call__", static_cast<Equal1>(&Equal_2::operator()));
