6d879d6619c132ecac53bb38dc6fc6499c7ae081
Efi Fogel <efifogel@gmail.com>
2021-10-26 20:29:20 +0300

    Started to add support for general polygons bounded by circle segment traits

---
+++ src/libs/cgalpy/lib/gps_circle_segment_traits_bindings.cpp
@@ -0,0 +1,33 @@
+// Copyright (c) 2019 Israel.
+// All rights reserved to Tel Aviv University.
+//
+// This file is private property of Tel Aviv University.
+//
+// Author(s): Nir Goren         <nirgoren@mail.tau.ac.il>
+//            Efi Fogel         <efifogel@gmail.com>
+
+#define BOOST_BIND_GLOBAL_PLACEHOLDERS
+
+#include <CGAL/Gps_circle_segment_traits_2.h>
+#include <CGAL/General_polygon_set_2.h>
+
+#include "CGALPY/general_polygon_set_2_types.hpp"
+#include "CGALPY/gps_2_concepts/export_GpsTraits_2.hpp"
+#include "CGALPY/gps_2_concepts/Gps_traits_classes.hpp"
+
+namespace bp = boost::python;
+
+bp::object export_arr_circle_segment_traits();
+
+bp::object export_gps_circle_segment_traits() {
+  auto traits = export_arr_circle_segment_traits();
+
+  // typedef bso2::Traits_2       GT;
+  // bp::scope traits_scope(traits);
+  // struct Concepts {
+  //   Gps_traits_classes<GT> m_traits_classes;
+  // };
+  // Concepts concepts;
+  // export_GpsTraits_2<GT, Copy_const_reference>(traits, concepts);
+  return traits;
+}
