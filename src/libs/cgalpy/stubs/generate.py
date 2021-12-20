5f42a17e9f28416355e52b559b5f6a8a7f023517
Nir Goren <nir.goren@mail.tau.ac.il>
2021-12-20 14:11:55 +0200

    Update stubs generation script

--- src/libs/cgalpy/stubs/generate.py
+++ src/libs/cgalpy/stubs/generate.py
@@ -1,7 +1,60 @@
 import json
 import os
+import re
 
 INDENTATION = '    '
+
+def replace_variables(s: str, variables_map: dict):
+    splits = s.split('@')
+    variables = [splits[i] for i in range(len(splits)) if i%2 == 1]
+    for variable in variables:
+        s = s.replace('@' + variable + '@', variables_map[variable])
+    return s
+
+def add_missing_fields_to_function(f: dict):
+    if "required" not in f:
+        f["required"] = True
+    if "variables" not in f:
+        f["variables"] = {}
+    if "return" not in f:
+        f["return"] = 'None'
+
+
+def add_missing_fields(d: dict):
+    if "required" not in d:
+        d["required"] = True
+    if "imports" not in d:
+        d["imports"] = []
+    if "typedefs" not in d:
+        d["typedefs"] = {}
+    if "models" not in d:
+        d["models"] = {}
+    if "concepts" not in d:
+        d["concepts"] = {}
+    if "classes" not in d:
+        d["classes"] = {}
+    if "methods" not in d:
+        d["methods"] = {}
+    for _, concept in d["concepts"].items():
+        add_missing_fields(concept)
+    for _, _class in d["classes"].items():
+        add_missing_fields(_class)
+    for _, method in d["methods"].items():
+        for overload in method:
+            add_missing_fields_to_function(overload)   
+        
+
+def filter_required(d: dict):
+    d["classes"] = {_class: d["classes"][_class] for _class in d["classes"] if d["classes"][_class]["required"]}
+    for class_name, class_def in d["classes"].items():
+        filter_required(class_def)
+    d["concepts"] = {concept: d["concepts"][concept] for concept in d["concepts"] if d["concepts"][concept]["required"]}
+    for concept, concept_def in d["concepts"].items():
+        filter_required(concept_def)
+    d["models"] = {concept: d["models"][concept] for concept in d["models"] if d["models"][concept]}
+    for method, overloads in d["methods"].items():
+        d["methods"][method] = [overload for overload in overloads if overload["required"]]
+    
 
 
 def merge_classes(c1, c2):
@@ -19,7 +72,7 @@ def merge_classes(c1, c2):
 
 
 def get_all_concepts(concept):
-    concepts = concept["concepts"]
+    concepts = concept["models"]
     res = json.loads(json.dumps(concepts))
     if concepts:
         for concept in concepts:
@@ -28,8 +81,8 @@ def get_all_concepts(concept):
 
 
 def resolve_class(class_def):
-    class_def["concepts"] = get_all_concepts(class_def)
-    for concept in class_def["concepts"]:
+    class_def["models"] = get_all_concepts(class_def)
+    for concept in class_def["models"]:
         merge_classes(class_def, module_concepts[concept])
 
     class_def["resolved"] = True
@@ -37,7 +90,7 @@ def resolve_class(class_def):
 
 def print_class(class_name, class_def, indent=0):
     print(INDENTATION*indent + 'class ' + class_name + '():')
-    # print(INDENT*indent + '#models: ' + str(class_def["concepts"]))
+    print(INDENTATION*indent + '#models: ' + str(class_def["concepts"]))
     for typedef, value in class_def["typedefs"].items():
         print(INDENTATION*(indent+1) + typedef + " = " + value)
     if class_def["typedefs"]:
@@ -62,12 +115,18 @@ if __name__ == "__main__":
     script_dir = os.path.dirname(__file__)
     file_path = os.path.join(script_dir, './definitions.json')
     f = open(file_path)
-    definitions = json.load(f)
+    definitions_string = f.read()
     f.close()
+    definitions_string = replace_variables(definitions_string, {})
+    definitions = json.loads(definitions_string)
+
+    add_missing_fields(definitions)
+    filter_required(definitions)
 
     module_concepts = definitions["concepts"]
     module_classes = definitions["classes"]
-    module_functions = definitions["functions"]
+    module_functions = definitions["methods"]
+    module_typedefs = definitions["typedefs"]
     imports = definitions["imports"]
 
     classes = module_classes.items()
@@ -83,6 +142,11 @@ if __name__ == "__main__":
 
     print()
     print()
+    
+    for typedef, value in module_typedefs.items():
+        print(typedef + " = " + value)
+    if module_typedefs:
+        print()
 
     for class_name, class_def in classes:
         print_class(class_name, class_def)
