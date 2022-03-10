8f52734e85a5cfb31b75b564185b5716a03ab098
Efi Fogel <efifogel@gmail.com>
2022-03-10 18:28:21 +0200

    Added license statement

--- src/libs/cgalpy/include/CGALPY/triangulation_3_types.hpp
+++ src/libs/cgalpy/include/CGALPY/triangulation_3_types.hpp
@@ -1,7 +1,8 @@
 // Copyright (c) 2019 Israel.
 // All rights reserved to Tel Aviv University.
 //
-// This file is private property of Tel Aviv University.
+// SPDX-License-Identifier: GPL-3.0-or-later.
+// Commercial use is authorized only through a concession contract to purchase a commercial license for CGAL.
 //
 // Author(s): Efi Fogel         <efifogel@gmail.com>
 
