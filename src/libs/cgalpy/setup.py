a0812e8f704c41f5dd73f1d6ddb54ef07220c2c5
Efi Fogel <efifogel@gmail.com>
2022-03-10 14:37:02 +0200

    Added version and license

---
+++ src/libs/cgalpy/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+package_data = {'': [@TARGET_DATA@] }
+setup(name='@TARGET_NAME@',
+      version='@SPHINX_TARGET_VERSION_MAJOR@.@SPHINX_TARGET_VERSION_MINOR@',
+      description='CGAL Python Bindings',
+      py_modules=['@TARGET_MODULES@'],
+      packages=[''],
+      platforms=['any'],
+      author='@TARGET_AUTHOR@',
+      author_email='@TARGET_EMAIL@',
+      url='@TARGET_URL@',
+      package_data=package_data,
+      license='LGPLv3+')
