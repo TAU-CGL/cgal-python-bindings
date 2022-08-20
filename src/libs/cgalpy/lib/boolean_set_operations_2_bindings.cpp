9e089e13d56238cb007a5eafe64bd9a30b9d8413
Efi Fogel <efifogel@gmail.com>
2022-08-20 16:46:19 +0300

    Fixed typos

--- src/libs/cgalpy/lib/boolean_set_operations_2_bindings.cpp
+++ src/libs/cgalpy/lib/boolean_set_operations_2_bindings.cpp
@@ -172,12 +172,12 @@ void export_boolean_set_operations_2(py::module_& m) {
   m.def("connect_holes", &bso2::connect_holes);
 #else
 
-  if (add_attr<Pgn>("General_polygon_2", parent)) return;
-  cs_pgn_c = py::class_<Pgn>(m, "General_polygon_2");
-  export_general_polygon_2<CS_pgn>(cs_pgn_c);
+  if (add_attr<Pgn>("General_polygon_2", m)) return;
+  auto cs_pgn_c = py::class_<Pgn>(m, "General_polygon_2");
+  export_general_polygon_2<Pgn>(cs_pgn_c);
 
-  if (add_attr<Pwh>("General_polygon_with_holes_2", parent)) return;
-  cs_pwh_c = py::class_<Pwh>(m, "General_polygon_with_holes_2");
+  if (add_attr<Pwh>("General_polygon_with_holes_2", m)) return;
+  auto cs_pwh_c = py::class_<Pwh>(m, "General_polygon_with_holes_2");
   export_general_polygon_with_holes_2<Pwh>(cs_pwh_c);
 
 #endif
