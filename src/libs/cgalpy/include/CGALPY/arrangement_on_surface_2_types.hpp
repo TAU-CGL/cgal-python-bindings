c4b4aba0a22319b4f60838cda4db1c125879a110
Efi Fogel <efifogel@gmail.com>
2022-08-29 18:49:08 +0300

    Port to nanobind

--- src/libs/cgalpy/include/CGALPY/arrangement_on_surface_2_types.hpp
+++ src/libs/cgalpy/include/CGALPY/arrangement_on_surface_2_types.hpp
@@ -110,7 +110,11 @@ typedef Arrangement_on_surface_2::Vertex_iterator          Vertex_iterator;
 typedef Arrangement_on_surface_2::Halfedge_iterator        Halfedge_iterator;
 typedef Arrangement_on_surface_2::Face_iterator            Face_iterator;
 
-typedef Arrangement_on_surface_2::Vertex_const_handle      Vertex_const_handle;
+typedef Arrangement_on_surface_2::Vertex_handle            Vertex_handle;
+typedef Arrangement_on_surface_2::Halfedge_handle          Halfedge_handle;
+typedef Arrangement_on_surface_2::Face_handle              Face_handle;
+
+  typedef Arrangement_on_surface_2::Vertex_const_handle    Vertex_const_handle;
 typedef Arrangement_on_surface_2::Halfedge_const_handle    Halfedge_const_handle;
 typedef Arrangement_on_surface_2::Face_const_handle        Face_const_handle;
 
@@ -126,9 +130,6 @@ typedef Arrangement_on_surface_2::Ccb_halfedge_circulator
   Ccb_halfedge_circulator;
 typedef Arrangement_on_surface_2::Halfedge_around_vertex_circulator
   Halfedge_around_vertex_circulator;
-typedef Arrangement_on_surface_2::Vertex_iterator          Vertex_iterator;
-typedef Arrangement_on_surface_2::Edge_iterator            Edge_iterator;
-typedef Arrangement_on_surface_2::Face_iterator            Face_iterator;
 
 }
 
--- src/python_scripts/cgalpy_examples/Arrangement_on_surface_2/batched_point_location.py
+++ src/python_scripts/cgalpy_examples/Arrangement_on_surface_2/batched_point_location.py
@@ -1,32 +1,53 @@
-//! \file examples/Arrangement_on_surface_2/batched_point_location.cpp
-// Answering a batched point-location query.
+#!/usr/bin/python3
+# export PYTHONPATH=...
 
-#include <list>
-#include <vector>
+import os
+import sys
+import importlib
+from construct_segment_arrangement import *
 
-#include <CGAL/basic.h>
-#include <CGAL/Arr_batched_point_location.h>
+if len(sys.argv) < 2:
+  sys.path.append(os.path.abspath('../precompiled'))
+  lib = 'CGALPY'
+else:
+  lib = sys.argv[1]
 
-#include "arr_inexact_construction_segments.h"
-#include "point_location_utils.h"
+CGALPY = importlib.import_module(lib)
+Ker = CGALPY.Ker
+Aos2 = CGALPY.Aos2
+Arrangement = Aos2.Arrangement_2
+Segment = Arrangement.Geometry_traits_2.Curve_2
+Point = Arrangement.Geometry_traits_2.Point_2
 
-typedef CGAL::Arr_point_location_result<Arrangement>    Point_location_result;
-typedef std::pair<Point, Point_location_result::Type>   Query_result;
+# Construct the arrangement.
+arr = Arrangement()
+# construct_segments_arr(arr)
+p0 = Point(3,2)
+p1 = Point(0,3)
+p2 = Point(2,5)
+p3 = Point(4,5)
+p4 = Point(6,3)
+p5 = Point(3,0)
+s1 = Segment(p1, p2)
+s2 = Segment(p2, p3)
+s3 = Segment(p3, p4)
+s4 = Segment(p4, p5)
+s5 = Segment(p5, p1)
 
-int main() {
-  // Construct the arrangement.
-  Arrangement arr;
-  construct_segments_arr(arr);
+arr.insert_in_face_interior(p0, arr.unbounded_face())
+e1 = arr.insert_in_face_interior(s1, arr.unbounded_face())
+e2 = arr.insert_from_left_vertex(s2, e1.target())
+e3 = arr.insert_from_left_vertex(s3, e2.target())
+e4 = arr.insert_from_right_vertex(s4, e3.target())
+arr.insert_at_vertices(s5, e4.target(), e1.source())
 
-  // Perform a batched point-location query.
-  std::vector<Point> points = {
-    Point(1, 4), Point(4, 3), Point(6, 3), Point(3, 2), Point(5, 2), Point(1, 0)
-  };
-  std::list<Query_result> results;
-  locate(arr, points.begin(), points.end(), std::back_inserter(results));
+# Aos2.insert(arr, Segment(Point(3,0), Point(0,3)))
+Aos2.draw(arr)
 
-  // Print the results.
-  for (auto it = results.begin(); it != results.end(); ++it)
-    print_point_location<Arrangement>(it->first, it->second);
-  return 0;
-}
+# Perform a batched point-location query.
+points = [Point(1, 4), Point(4, 3), Point(6, 3), Point(3, 2), Point(5, 2), Point(1, 0)]
+res = Aos2.locate(arr, points)
+
+# Print the results.
+for element in res:
+  print(element)
