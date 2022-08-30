f3277a47d011c495c8116963da382069a5394bd9
Efi Fogel <efifogel@gmail.com>
2022-08-30 16:16:01 +0300

    Circle segment port

--- src/python_scripts/cgalpy_examples/Arrangement_on_surface_2/circles.py
+++ src/python_scripts/cgalpy_examples/Arrangement_on_surface_2/circles.py
@@ -1,31 +1,41 @@
-//! \file examples/Arrangement_on_surface_2/circles.cpp
-// Constructing an arrangement of circles using the circle-segment traits.
+#!/usr/bin/python3
+# export PYTHONPATH=...
+import os
+import sys
+import importlib
+import arr_print as ap
 
-#include "arr_circular.h"
+if len(sys.argv) < 2:
+    sys.path.append(os.path.abspath('../precompiled'))
+    lib = 'CGALPY_kerEpec_aos2Alg'
+else:
+    lib = sys.argv[1]
 
-int main() {
-  Arrangement arr;
+CGALPY = importlib.import_module(lib)
+Ker = CGALPY.Ker
+FT = Ker.FT
+Aos2 = CGALPY.Aos2
+Arrangement_2 = Aos2.Arrangement_2
+arr = Arrangement_2()
+Curve = Arrangement_2.Curve_2
 
-  // Create a circle centered at the origin with radius 5 (C1).
-  insert(arr, Curve(Circle(Rational_point(0, 0), Number_type(25))));
+# # Create a circle centered at the origin with radius 5 (C1).
+Aos2.insert(arr, Curve(Ker.Circle_2(Ker.Point_2(0, 0), FT(25))));
 
-  // Create a circle centered at (7,7) with radius 5 (C2).
-  insert(arr, Curve(Circle(Rational_point(7, 7), Number_type(25))));
+# # Create a circle centered at (7,7) with radius 5 (C2).
+Aos2.insert(arr, Curve(Ker.Circle_2(Ker.Point_2(7, 7), FT(25))));
 
-  // Create a circle centered at (4,-0.5) with radius 3.5 (= 7/2) (C3).
-  Rational_point c3(4, Number_type(-1) / Number_type(2));
-  insert(arr, Curve(Circle(c3, Number_type(49) / Number_type(4))));
+# # Create a circle centered at (4,-0.5) with radius 3.5 (= 7/2) (C3).
+c3 = Ker.Point_2(4, FT(-1) / FT(2))
+Aos2.insert(arr, Curve(Ker.Circle_2(c3, FT(49) / FT(4))))
 
-  // Locate the vertex with maximal degree.
-  auto vit = arr.vertices_begin();
-  Arrangement::Vertex_const_handle v_max = vit;
-  for (++vit; vit != arr.vertices_end(); ++vit)
-    if (vit->degree() > v_max->degree()) v_max = vit;
+# Locate the vertex with maximal degree.
+vertices = arr.vertices()
+v_max = next(vertices)
+print(v_max.point())
+v: Arrangement_2.Vertex
+for v in vertices:
+  if (v.degree() > v_max.degree()): v_max = v
 
-  // Locate the vertex with maximum degree.
-
-  std::cout << "The vertex with maximal degree in the arrangement is: "
-            << "v_max = (" << v_max->point() << ") "
-            << "with degree " << v_max->degree() << "." << std::endl;
-  return 0;
-}
+print('The vertex with maximal degree in the arrangement is: ' +
+      'v_max = ({}) with degree {}.'.format(v_max.point(), v_max.degree()))
