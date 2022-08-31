3e0fd817b4835c2e2828540e3f199a50ff6da172
Efi Fogel <efifogel@gmail.com>
2022-09-01 01:20:18 +0300

    Port to nanobind

--- src/python_scripts/cgalpy_examples/Arrangement_on_surface_2/global_insertion.py
+++ src/python_scripts/cgalpy_examples/Arrangement_on_surface_2/global_insertion.py
@@ -1,26 +1,36 @@
-//! \file examples/Arrangement_on_surface_2/global_insertion.cpp
-// Using the global insertion functions (incremental and aggregated).
+#!/usr/bin/python3
+# export PYTHONPATH=...
+import os
+import sys
+import importlib
+from arr_print import *
 
-#include "arr_exact_construction_segments.h"
-#include "arr_print.h"
+if len(sys.argv) < 2:
+  sys.path.append(os.path.abspath('../precompiled'))
+  lib = 'CGALPY'
+else:
+  lib = sys.argv[1]
 
-int main() {
-  Segment S1[] = {Segment(Point(1, 3), Point(4, 6)),
-                  Segment(Point(1, 3), Point(6, 3)),
-                  Segment(Point(1, 3), Point(4, 0)),
-                  Segment(Point(4, 6), Point(6, 3)),
-                  Segment(Point(4, 0), Point(6, 3))};
-  Segment s = Segment(Point(0, 3), Point(4, 3));
-  Segment S2[] = {Segment(Point(0, 5), Point(6, 6)),
-                  Segment(Point(0, 4), Point(6, 5)),
-                  Segment(Point(0, 2), Point(6, 1)),
-                  Segment(Point(0, 1), Point(6, 0)),
-                  Segment(Point(6, 1), Point(6, 5))};
+CGALPY = importlib.import_module(lib)
+Aos2 = CGALPY.Aos2
+Arrangement = Aos2.Arrangement_2
+Point = Arrangement.Geometry_traits_2.Point_2
+Segment = Arrangement.Geometry_traits_2.X_monotone_curve_2
 
-  Arrangement arr;
-  insert_non_intersecting_curves(arr, S1, S1 + sizeof(S1)/sizeof(Segment));
-  insert(arr, s);                                     // 1 incremental
-  insert(arr, S2, S2 + sizeof(S2)/sizeof(Segment));   // 5 aggregate
-  print_arrangement_size(arr);
-  return 0;
-}
+S1 = [ Segment(Point(1, 3), Point(4, 6)),
+       Segment(Point(1, 3), Point(6, 3)),
+       Segment(Point(1, 3), Point(4, 0)),
+       Segment(Point(4, 6), Point(6, 3)),
+       Segment(Point(4, 0), Point(6, 3)) ]
+s = Segment(Point(0, 3), Point(4, 3))
+S2 = [ Segment(Point(0, 5), Point(6, 6)),
+       Segment(Point(0, 4), Point(6, 5)),
+       Segment(Point(0, 2), Point(6, 1)),
+       Segment(Point(0, 1), Point(6, 0)),
+       Segment(Point(6, 1), Point(6, 5)) ]
+
+arr = Arrangement()
+Aos2.insert_non_intersecting_curves(arr, S1)
+Aos2.insert(arr, s);   	# 1 incremental
+Aos2.insert(arr, S2)   	# 5 aggregate
+print_arrangement_size(arr)
