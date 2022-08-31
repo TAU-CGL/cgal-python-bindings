eb64058fcc09ae1405a1c9458784fd0bb7ac582c
Efi Fogel <efifogel@gmail.com>
2022-08-31 20:26:54 +0300

    Circular arcs port

--- src/python_scripts/cgalpy_examples/Arrangement_on_surface_2/circles.py
+++ src/python_scripts/cgalpy_examples/Arrangement_on_surface_2/circles.py
@@ -3,7 +3,7 @@
 import os
 import sys
 import importlib
-import arr_print as ap
+from arr_print import *
 
 if len(sys.argv) < 2:
     sys.path.append(os.path.abspath('../precompiled'))
@@ -13,27 +13,29 @@ else:
 
 CGALPY = importlib.import_module(lib)
 Ker = CGALPY.Ker
-FT = Ker.FT
+Number_type = Ker.FT
+Circle = Ker.Circle_2
+Rational_point = Ker.Point_2
 Aos2 = CGALPY.Aos2
-Arrangement_2 = Aos2.Arrangement_2
-arr = Arrangement_2()
-Curve = Arrangement_2.Curve_2
+Arrangement = Aos2.Arrangement_2
+arr = Arrangement()
+Curve = Arrangement.Curve_2
 
 # # Create a circle centered at the origin with radius 5 (C1).
-Aos2.insert(arr, Curve(Ker.Circle_2(Ker.Point_2(0, 0), FT(25))));
+Aos2.insert(arr, Curve(Circle(Rational_point(0, 0), Number_type(25))));
 
 # # Create a circle centered at (7,7) with radius 5 (C2).
-Aos2.insert(arr, Curve(Ker.Circle_2(Ker.Point_2(7, 7), FT(25))));
+Aos2.insert(arr, Curve(Circle(Rational_point(7, 7), Number_type(25))));
 
 # # Create a circle centered at (4,-0.5) with radius 3.5 (= 7/2) (C3).
-c3 = Ker.Point_2(4, FT(-1) / FT(2))
-Aos2.insert(arr, Curve(Ker.Circle_2(c3, FT(49) / FT(4))))
+c3 = Rational_point(4, Number_type(-1) / Number_type(2))
+Aos2.insert(arr, Curve(Circle(c3, Number_type(49) / Number_type(4))))
 
 # Locate the vertex with maximal degree.
 vertices = arr.vertices()
 v_max = next(vertices)
 print(v_max.point())
-v: Arrangement_2.Vertex
+v: Arrangement.Vertex
 for v in vertices:
   if (v.degree() > v_max.degree()): v_max = v
 
--- src/python_scripts/cgalpy_examples/Arrangement_on_surface_2/circular_arcs.py
+++ src/python_scripts/cgalpy_examples/Arrangement_on_surface_2/circular_arcs.py
@@ -1,60 +1,76 @@
-//! \file examples/Arrangement_on_surface_2/circular_arc.cpp
-// Constructing an arrangement of various circular arcs and line segments.
+#!/usr/bin/python3
+# export PYTHONPATH=...
+import os
+import sys
+import importlib
+from arr_print import *
 
-#include "arr_circular.h"
-#include "arr_print.h"
+if len(sys.argv) < 2:
+    sys.path.append(os.path.abspath('../precompiled'))
+    lib = 'CGALPY_kerEpec_aos2Alg'
+else:
+    lib = sys.argv[1]
 
-int main() {
-  std::list<Curve> curves;
+CGALPY = importlib.import_module(lib)
+Ker = CGALPY.Ker
+Number_type = Ker.FT
+Circle = Ker.Circle_2
+Segment = Ker.Segment_2
+Rational_point = Ker.Point_2
+Aos2 = CGALPY.Aos2
+Arrangement = Aos2.Arrangement_2
+Point = Arrangement.Point_2
+Curve = Arrangement.Curve_2
+CoordNT = Aos2.CoordNT
 
-  // Create a circle (C1) centered at the origin with squared radius 2.
-  curves.push_back(Curve(Circle(Rational_point(0, 0), Number_type(2))));
+curves = []
 
-  // Create a circle (C2) centered at (2, 3) with radius 3/2. Note that
-  // as the radius is rational we use a different curve constructor.
-  Number_type three_halves = Number_type(3) / Number_type(2);
-  curves.push_back(Curve(Rational_point(2, 3), three_halves));
+# Create a circle (C1) centered at the origin with squared radius 2.
+curves.append(Curve(Circle(Rational_point(0, 0), Number_type(2))))
 
-  // Create a segment (C3) of the line (y = x) with rational endpoints.
-  Segment s3 = Segment(Rational_point(-2, -2), Rational_point(2, 2));
-  curves.push_back(Curve(s3));
+# Create a circle (C2) centered at (2, 3) with radius 3/2. Note that
+# as the radius is rational we use a different curve constructor.
+three_halves = Number_type(3) / Number_type(2)
+curves.append(Curve(Rational_point(2, 3), three_halves))
 
-  // Create a line segment (C4) with the same supporting line (y = x), but
-  // having one endpoint with irrational coordinates.
-  CoordNT sqrt_15 = CoordNT(0, 1, 15); // = sqrt(15)
-  curves.push_back(Curve(s3.supporting_line(),
-                         Point(3, 3), Point(sqrt_15, sqrt_15)));
+# Create a segment (C3) of the line (y = x) with rational endpoints.
+s3 = Segment(Rational_point(-2, -2), Rational_point(2, 2))
+curves.append(Curve(s3))
 
-  // Create a circular arc (C5) that is the upper half of the circle centered at
-  // (1, 1) with squared radius 3. Create the circle with clockwise orientation,
-  // so the arc is directed from (1 - sqrt(3), 1) to (1 + sqrt(3), 1).
-  Rational_point c5(1, 1);
-  Circle circ5(c5, 3, CGAL::CLOCKWISE);
-  CoordNT one_minus_sqrt_3(1, -1, 3);
-  CoordNT one_plus_sqrt_3(1, 1, 3);
-  Point s5(one_minus_sqrt_3, CoordNT(1));
-  Point t5(one_plus_sqrt_3, CoordNT(1));
-  curves.push_back(Curve(circ5, s5, t5));
+# Create a line segment (C4) with the same supporting line (y = x), but
+# having one endpoint with irrational coordinates.
+sqrt_15 = CoordNT(0, 1, 15) # = sqrt(15)
+curves.append(Curve(s3.supporting_line(),
+                    Point(3, 3), Point(sqrt_15, sqrt_15)))
 
-  // Create an arc (C6) of the unit circle, directed clockwise from
-  // (-1/2, sqrt(3)/2) to (1/2, sqrt(3)/2).
-  // The supporting circle is oriented accordingly.
-  Rational_point c6(0, 0);
-  Number_type half = Number_type(1) / Number_type(2);
-  CoordNT sqrt_3_div_2(Number_type(0), half, 3);
-  Point s6(-half, sqrt_3_div_2);
-  Point t6(half, sqrt_3_div_2);
-  curves.push_back(Curve(c6, 1, CGAL::CLOCKWISE, s6, t6));
+# Create a circular arc (C5) that is the upper half of the circle centered at
+# (1, 1) with squared radius 3. Create the circle with clockwise orientation,
+# so the arc is directed from (1 - sqrt(3), 1) to (1 + sqrt(3), 1).
+c5 = Rational_point (1, 1)
+circ5 = Circle(c5, 3, Ker.CLOCKWISE)
+one_minus_sqrt_3 = CoordNT(1, -1, 3)
+one_plus_sqrt_3 = CoordNT(1, 1, 3)
+s5 = Point(one_minus_sqrt_3, CoordNT(1))
+t5 = Point(one_plus_sqrt_3, CoordNT(1))
+curves.append(Curve(circ5, s5, t5))
 
-  // Create a circular arc (C7) defined by two endpoints and a midpoint,
-  // all having rational coordinates. This arc is the upper right
-  // quarter of a circle centered at the origin with radius 5.
-  curves.push_back(Curve(Rational_point(0, 5), Rational_point(3, 4),
-                         Rational_point(5, 0)));
+# Create an arc (C6) of the unit circle, directed clockwise from
+# (-1/2, sqrt(3)/2) to (1/2, sqrt(3)/2).
+# The supporting circle is oriented accordingly.
+c6 = Rational_point(0, 0)
+half = Number_type(1) / Number_type(2)
+sqrt_3_div_2 = CoordNT(Number_type(0), half, 3)
+s6 = Point(-half, sqrt_3_div_2)
+t6 = Point(half, sqrt_3_div_2)
+curves.append(Curve(c6, 1, Ker.CLOCKWISE, s6, t6))
 
-  // Construct the arrangement of the curves and print its size.
-  Arrangement  arr;
-  insert(arr, curves.begin(), curves.end());
-  print_arrangement_size(arr);
-  return 0;
-}
+# Create a circular arc (C7) defined by two endpoints and a midpoint,
+# all having rational coordinates. This arc is the upper right
+# quarter of a circle centered at the origin with radius 5.
+curves.append(Curve(Rational_point(0, 5), Rational_point(3, 4),
+                    Rational_point(5, 0)))
+
+# Construct the arrangement of the curves and print its size.
+arr = Arrangement()
+Aos2.insert(arr, curves)
+print_arrangement_size(arr)
