a128c6824105eb2b6b5a4f8fa75f54b08a70e05e
Efi Fogel <efifogel@gmail.com>
2022-09-05 19:39:45 +0300

    Port

--- src/python_scripts/cgalpy_examples/Arrangement_on_surface_2/dual_with_data.py
+++ src/python_scripts/cgalpy_examples/Arrangement_on_surface_2/dual_with_data.py
@@ -1,51 +1,56 @@
-//! \file examples/Arrangement_on_surface_2/dual_lines.cpp
-// Checking whether there are three collinear points in a given input set
-// using the arrangement of the dual lines.
+from arr_print import *
+from read_objects import *
 
-#include <algorithm>
+if len(sys.argv) < 2:
+  sys.path.append(os.path.abspath('../precompiled'))
+  lib = 'CGALPY'
+else:
+  lib = sys.argv[1]
 
-#include <CGAL/basic.h>
+CGALPY = importlib.import_module(lib)
+Aos2 = CGALPY.Aos2
+Arrangement = Aos2.Arrangement_2
+Traits = Aos2.Arr_linear_traits_2
+Xcv = Traits.X_monotone_curve_2
+Point = Traits.Point_2
+Line = Traits.Line_2
+Ker = CGALPY.Ker
+
 #include <CGAL/Arr_curve_data_traits_2.h>
-
 #include "arr_linear.h"
-#include "read_objects.h"
 
-typedef CGAL::Arr_curve_data_traits_2<Traits, size_t>   Data_traits;
-typedef Data_traits::X_monotone_curve_2                 Data_x_monotone_curve_2;
-typedef CGAL::Arrangement_2<Data_traits>                Data_arrangement;
+# typedef CGAL::Arr_curve_data_traits_2<Traits, size_t>   Data_traits;
+# typedef Data_traits::X_monotone_curve_2                 Data_x_monotone_curve_2;
+# typedef CGAL::Arrangement_2<Data_traits>                Data_arrangement;
 
-int main(int argc, char* argv[]) {
-  // Get the name of the input file from the command line, or use the default
-  // points.dat file if no command-line parameters are given.
-  const char* filename = (argc > 1) ? argv[1] : "coll_points.dat";
+# Get the name of the input file from the command line, or use the default
+# points.dat file if no command-line parameters are given.
+try:
+  filename = argv[1]
+except:
+  filename = 'coll_points.dat'
 
-  std::vector<Point> points;
-  read_objects<Point>(filename, std::back_inserter(points));
-  std::vector<Data_x_monotone_curve_2> dual_lines(points.size());
-  size_t k{0};
-  std::transform(points.begin(), points.end(), dual_lines.begin(),
-                 [&](const Point& p) {
-                   Line dual_line(p.x(), -1, -(p.y()));
-                   return Data_x_monotone_curve_2(dual_line, k++);
-                 });
+points = read_objects(Point, filename)
+dual_lines = []
+k = 0
+std::transform(points.begin(), points.end(), dual_lines.begin(),
+               [&](const Point& p) {
+                 Line dual_line(p.x(), -1, -(p.y()))
+                 return Data_x_monotone_curve_2(dual_line, k++)
+               })
 
-  // Construct the dual arrangement by aggregately inserting the lines.
-  Data_arrangement arr;
-  insert(arr, dual_lines.begin(), dual_lines.end());
+# Construct the dual arrangement by aggregately inserting the lines.
+arr = Arrangement()
+Aos2.insert(arr, dual_lines)
 
-  // Look for vertices whose degree is greater than 4.
-  for (auto vit = arr.vertices_begin(); vit != arr.vertices_end(); ++vit) {
-    if (vit->degree() > 4) {
-      // There should be vit->degree()/2 lines intersecting at the current
-      // vertex. We print their primal points and their indices.
-      auto circ = vit->incident_halfedges();
-      for (size_t d = 0; d < vit->degree() / 2; ++d) {
-        k = circ->curve().data();     // The index of the primal point.
-        std::cout << "Point no. " << k+1 << ": (" << points[k] << "), ";
-        ++circ;
-      }
-      std::cout << "are collinear.\n";
-    }
-  }
-  return 0;
-}
+# Look for vertices whose degree is greater than 4.
+v: Arrangement.Vertex
+for v in arr.vertices():
+  if vit.degree() > 4:
+    # There should be vit.degree()/2 lines intersecting at the current
+    # vertex. We print their primal points and their indices.
+    circ = v.incident_halfedges()
+    for d in range(v.degree() / 2):
+      k = next(circ).curve().data()  # The index of the primal point.
+      print('Point no. {}: ({}), '.format(k+1, points[k]), end='')
+    print("are collinear.")
