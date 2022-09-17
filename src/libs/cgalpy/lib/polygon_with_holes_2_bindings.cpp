729452bf8aac7556b958db2572bd684b7299630d
Efi Fogel <efifogel@gmail.com>
2022-09-17 23:14:21 +0300

    Fixed Polygon_with_holes_2 bindings (as it derives from General_polygon_with_holes_2)

--- src/libs/cgalpy/lib/polygon_with_holes_2_bindings.cpp
+++ src/libs/cgalpy/lib/polygon_with_holes_2_bindings.cpp
@@ -14,7 +14,8 @@
 #include "CGALPY/python_iterator_templates.hpp"
 #include "CGALPY/add_insertion.hpp"
 #include "CGALPY/stl_input_iterator.hpp"
-#include "CGALPY/make_iterator.hpp"
+#include "CGALPY/add_attr.hpp"
+#include "CGALPY/export_general_polygon_with_holes_2.hpp"
 
 namespace py = nanobind;
 
@@ -30,38 +31,31 @@ void init_polygon_with_holes_2(Polygon_with_holes_2& pwh, Polygon_2& p,
 
 }
 
-//
+/*! Export `CGAL::Polygon_with_holes_2<>`, which derives from
+ * `CGAL::General_polygon_with_holes_2<>`
+ */
 void export_polygon_with_holes_2(py::module_& m) {
   using Pgn = pol2::Polygon_2;
   using Pwh = pol2::Polygon_with_holes_2;
+  using Gpwh = pol2::General_polygon_with_holes_2;
 
-  py::class_<Pwh> pwh_c(m, "Polygon_with_holes_2");
-  pwh_c.def(py::init<>())
-    .def(py::init<Pgn&>())
-    .def("__init__", &pol2::init_polygon_with_holes_2)
-    .def("is_unbounded", &Pwh::is_unbounded)
+  if (! add_attr<Gpwh>(m, "General_polygon_with_holes_2")) {
+    py::class_<Gpwh> gpwh_c(m, "General_polygon_with_holes_2");
+    export_general_polygon_with_holes_2(gpwh_c);
+  }
 
-    // Use `py::overload_cast` to cast overloaded functions.
-    // 1. As a convention, add the suffix `_mutable` to the mutable version.
-    // 2. Wrap the mutable method with the `reference_internal` call policy.
-    // 3. Add the `const_` tag to the overloaded const function, as the
-    //    overloading is based on constness.
-    .def("outer_boundary_mutable", py::overload_cast<>(&Pwh::outer_boundary),
-         py::rv_policy::reference_internal)
-    .def("outer_boundary", py::overload_cast<>(&Pwh::outer_boundary, py::const_))
-    .def("number_of_holes", &Pwh::number_of_holes)
-    .def("bbox", &Pwh::bbox)
-    .def(py::self == py::self)
-    .def(py::self != py::self)
-    ;
+  if (! add_attr<Pwh>(m, "Polygon_with_holes_2")) {
+    py::class_<Pwh, Gpwh> pwh_c(m, "Polygon_with_holes_2");
+    pwh_c.def(py::init<>())
+      .def(py::init<Pgn&>())
+      .def("__init__", &pol2::init_polygon_with_holes_2)
 
-  using Hci = Pwh::Hole_const_iterator;
-  add_iterator<Hci, Hci, const Pgn&>("Hole_iterator", pwh_c);
-  pwh_c.def("holes",
-              [] (const Pwh& pwh)
-              { return make_iterator(pwh.holes_begin(), pwh.holes_end()); },
-              py::keep_alive<0, 1>());
+      .def("bbox", &Pwh::bbox)
+      .def(py::self == py::self)
+      .def(py::self != py::self)
+      ;
 
-  add_insertion(pwh_c, "__str__");
-  add_insertion(pwh_c, "__repr__");
+    add_insertion(pwh_c, "__str__");
+    add_insertion(pwh_c, "__repr__");
+  }
 }
