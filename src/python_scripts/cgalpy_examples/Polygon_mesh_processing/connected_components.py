5ba2d92f0e2eedfd67aa57e2bf0371b0d6ef17a2
Efi Fogel <efifogel@gmail.com>
2022-09-22 01:19:00 +0300

    Added support for Polygon_mesh_processing

---
+++ src/python_scripts/cgalpy_examples/Polygon_mesh_processing/connected_components.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+# export PYTHONPATH=...
+import os
+import sys
+import importlib
+
+if len(sys.argv) < 2:
+    sys.path.append(os.path.abspath('../precompiled'))
+    lib = 'CGALPY'
+else:
+    lib = sys.argv[1]
+
+CGALPY = importlib.import_module(lib)
+Ker = CGALPY.Ker
+Point_3 = Ker.Point_3
+Sm = CGALPY.Sm
+Pmp = CGALPY.Pmp
+
+p1 = Point_3(0, 0, 0)
+p2 = Point_3(2, 0, 0)
+p3 = Point_3(1, 1, 1)
+p4 = Point_3(1, 0, 2)
+
+mesh = Sm.make_tetrahedron(p1, p2, p3, p4)
+fcp = {}
+params = {}
+res = Pmp.connected_components(mesh, fcp)
+print(res)
