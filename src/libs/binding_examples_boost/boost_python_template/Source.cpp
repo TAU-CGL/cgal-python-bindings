a55ae01e825a61f557c50ad7be9373a5a7f0c720
Efi Fogel <efifogel@gmail.com>
2022-11-27 02:45:32 +0200

    Moved to tcgl/src/cgal_pythn_bindings/libs

--- src/libs/binding_examples_boost/boost_python_template/Source.cpp
+++
@@ -1,8 +0,0 @@
-#define BOOST_PYTHON_STATIC_LIB
-#define CGAL_HEADER_ONLY 1
-#include <boost/python.hpp>
-
-BOOST_PYTHON_MODULE(Source)
-{
-	using namespace boost::python;
-}
--- src/libs/binding_examples_pybind11/pybind11_template/Source.cpp
+++
@@ -1,8 +0,0 @@
-#define CGAL_HEADER_ONLY 1
-#include <pybind11/pybind11.h>
-namespace py = pybind11;
-
-PYBIND11_MODULE(Source, m)
-{
-	using namespace py;
-}
