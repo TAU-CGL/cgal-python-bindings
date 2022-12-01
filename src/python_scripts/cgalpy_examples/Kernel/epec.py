f6db8056297581382b638430932c41964a1af129
Efi Fogel <efifogel@gmail.com>
2022-12-01 17:26:56 +0200

    1st meaningful revision

--- src/python_scripts/cgalpy_examples/Kernel/epec.py
+++ src/python_scripts/cgalpy_examples/Kernel/epec.py
@@ -15,9 +15,18 @@ CGALPY = importlib.import_module(lib)
 Ker = CGALPY.Ker
 Point_2 = Ker.Point_2
 Kernel = Ker.Kernel
-# eq_2 =  Kernel.equal_2_object()
-# cc_in_between_2 = Kernel.counterclockwise_in_between_2_object()
-
-Counterclockwise_in_between_2 = Kernel.Counterclockwise_in_between_2
-p = Point_2(1, -1,)
-# p = Point_2(FT(1), FT(-1)) # error: FT is not defined for an inexact kernel
+kernel = Kernel()
+cc_in_between_2 = kernel.counterclockwise_in_between_2_object()
+p0 = Point_2(0,0)
+p1 = Point_2(1,0)
+p2 = Point_2(1,1)
+p3 = Point_2(0,1)
+v1 = p1 - p0
+v2 = p2 - p0
+v3 = p3 - p0
+Direction_2 = Ker.Direction_2
+d1 = Direction_2(v1)
+d2 = Direction_2(v2)
+d3 = Direction_2(v3)
+rc = cc_in_between_2(d2, d1, d3)
+print(rc)
