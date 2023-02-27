8702fc0d9459d38c8d0959a1cc9c3f5c2f8aba67
Efi Fogel <efifogel@gmail.com>
2023-02-27 13:48:26 +0200

    Used glob instead of nested loop

--- python-build/setup.py
+++ python-build/setup.py
@@ -1,18 +1,11 @@
 from distutils.core import setup
 import os
 import sys
+import glob
 
 SEARCH_DIR = '.'
 
-files = []
-for file in os.listdir('.'):
-      if '.so' in file or '.pyi' in file:
-            files.append(file)
-      if os.path.isdir(file):
-            for file2 in os.listdir(file):
-                  if '.pyi' in file2:
-                        files.append(os.path.join(file, file2))
-
+files = glob.glob('./*.so') + glob.glob('./*.pyi') + glob.glob('./*/*.pyi')
 package_data = {'': files}
 print(package_data)
 
