377bd885f592e1d80fd271568bad11b26a5b8017
Efi Fogel <efifogel@gmail.com>
2023-10-25 15:27:19 +0300

    Fixed typo

--- cmake/modules/visibility_2_options.cmake
+++ cmake/modules/visibility_2_options.cmake
@@ -4,7 +4,7 @@ set(VISIBILITY_2_OPTIONS_FILE_INCLUDED)
 # Regularization tag
 set(CGALPY_VIS2_REGULARIZATION_CATEGORY false CACHE BOOL "The regularization category tag")
 
-function(select_alpha_shape_2_comparison)
+function(select_visibility_2_comparison)
   if(${CGALPY_VIS2_REGULARIZATION_CATEGORY})
     add_definitions(-DCGALPY_VIS2_REGULARIZATION_CATEGORY=)
   endif()
@@ -14,6 +14,7 @@ endfunction()
 function(select_visibility_2)
   if(${CGALPY_VISIBILITY_2_BINDINGS})
     add_definitions(-DCGALPY_VISIBILITY_2_BINDINGS=)
+    select_visibility_2_comparison()
   endif()
 endfunction()
 
