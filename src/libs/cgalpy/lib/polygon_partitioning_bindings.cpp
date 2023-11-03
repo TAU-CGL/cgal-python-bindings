42230edd6ca643862410fd157eb92fb138a8ef3c
Efi Fogel <efifogel@gmail.com>
2023-11-03 11:10:12 +0200

    Updated

--- src/libs/cgalpy/lib/polygon_partitioning_bindings.cpp
+++ src/libs/cgalpy/lib/polygon_partitioning_bindings.cpp
@@ -9,6 +9,8 @@
 
 #include <nanobind/nanobind.h>
 
+#include <boost/iterator/function_output_iterator.hpp>
+
 #include <CGAL/partition_2.h>
 
 #include "CGALPY/polygon_partitioning_types.hpp"
@@ -20,87 +22,74 @@ namespace py = nanobind;
 namespace pp2 {
 
 //
-void approx_convex_partition_2(const Polygon_2& p, py::list& res) {
-  auto v = std::vector<Polygon_2>();
-  CGAL::approx_convex_partition_2(p.vertices_begin(), p.vertices_end(),
-                                  std::back_inserter(v));
-  for (auto c_polygon : v) res.append(c_polygon);
-}
+// void approx_convex_partition_2(const Polygon_2& p, py::list& res) {
+//   auto v = std::vector<Polygon_2>();
+//   CGAL::approx_convex_partition_2(p.vertices_begin(), p.vertices_end(),
+//                                   std::back_inserter(v));
+//   for (auto c_polygon : v) res.append(c_polygon);
+// }
 
 //
-void greene_approx_convex_partition_2(const Polygon_2& p, py::list& res) {
-  auto v = std::vector<Polygon_2>();
-  CGAL::greene_approx_convex_partition_2(p.vertices_begin(), p.vertices_end(),
-                                         std::back_inserter(v));
-  for (auto c_polygon : v) res.append(c_polygon);
-}
+// void greene_approx_convex_partition_2(const Polygon_2& p, py::list& res) {
+//   auto v = std::vector<Polygon_2>();
+//   CGAL::greene_approx_convex_partition_2(p.vertices_begin(), p.vertices_end(),
+//                                          std::back_inserter(v));
+//   for (auto c_polygon : v) res.append(c_polygon);
+// }
 
 //
-void optimal_convex_partition_2(const Polygon_2& p, py::list& res) {
-  auto v = std::vector<Polygon_2>();
-  CGAL::optimal_convex_partition_2(p.vertices_begin(), p.vertices_end(),
-                                   std::back_inserter(v));
-  for (auto c_polygon : v) res.append(c_polygon);
-}
+// void optimal_convex_partition_2(const Polygon_2& p, py::list& res) {
+//   auto v = std::vector<Polygon_2>();
+//   CGAL::optimal_convex_partition_2(p.vertices_begin(), p.vertices_end(),
+//                                    std::back_inserter(v));
+//   for (auto c_polygon : v) res.append(c_polygon);
+// }
 
-//
-void y_monotone_partition_2(const Polygon_2& p, py::list& res) {
-  auto v = std::vector<Polygon_2>();
-  CGAL::y_monotone_partition_2(p.vertices_begin(), p.vertices_end(),
-                               std::back_inserter(v));
-  for (auto ym_polygon : v) res.append(ym_polygon);
-}
+// //
+// void y_monotone_partition_2(const Polygon_2& p, py::list& res) {
+//   auto v = std::vector<Polygon_2>();
+//   CGAL::y_monotone_partition_2(p.vertices_begin(), p.vertices_end(),
+//                                std::back_inserter(v));
+//   for (auto ym_polygon : v) res.append(ym_polygon);
+// }
 
 /*! Bind partition is valid.
- * \todo for some reason using stl_input_iterator does not work. Instead we
- * introduce and intermediate array.
  */
-bool partition_is_valid_2(const Polygon_2& p, const py::list& lst) {
-#if 1
-  std::vector<Polygon_2> pgns;
-  for (auto it = lst.begin(); it != lst.end(); ++it)
-    pgns.push_back(py::cast<Polygon_2>(*it));
-  auto begin = pgns.begin();
-  auto end = pgns.end();
-#else
-  auto begin = stl_input_iterator<Polygon_2>(lst);
-  auto end = stl_input_iterator<Polygon_2>(lst, false);
-#endif
-  return CGAL::partition_is_valid_2(p.vertices_begin(), p.vertices_end(),
-                                    begin, end);
-}
+// bool partition_is_valid_2(const Polygon_2& p, const py::list& lst) {
+//   auto begin = stl_input_iterator<Polygon_2>(lst);
+//   auto end = stl_input_iterator<Polygon_2>(lst, false);
+//   return CGAL::partition_is_valid_2(p.vertices_begin(), p.vertices_end(),
+//                                     begin, end, traits);
+// }
 
 //
-bool convex_partition_is_valid_2(Polygon_2& p, const py::list& lst) {
-#if 1
-  std::vector<Polygon_2> pgns;
-  for (auto it = lst.begin(); it != lst.end(); ++it)
-    pgns.push_back(py::cast<Polygon_2>(*it));
-  auto begin = pgns.begin();
-  auto end = pgns.end();
-#else
-  auto begin = stl_input_iterator<Polygon_2>(lst);
-  auto end = stl_input_iterator<Polygon_2>(lst, false);
-#endif
-  return CGAL::convex_partition_is_valid_2(p.vertices_begin(), p.vertices_end(),
-                                           begin, end);
-}
+// bool convex_partition_is_valid_2(Polygon_2& p, const py::list& lst) {
+//   auto begin = stl_input_iterator<Polygon_2>(lst);
+//   auto end = stl_input_iterator<Polygon_2>(lst, false);
+//   return CGAL::convex_partition_is_valid_2(p.vertices_begin(), p.vertices_end(),
+//                                            begin, end, traits);
+// }
 
 //
-bool y_monotone_partition_is_valid_2(Polygon_2& p, const py::list& lst) {
-#if 1
-  std::vector<Polygon_2> pgns;
-  for (auto it = lst.begin(); it != lst.end(); ++it)
-    pgns.push_back(py::cast<Polygon_2>(*it));
+bool y_monotone_partition_is_valid_2(const Polygon_2& p, const py::list& lst) {
+  // The following is completely dumb!
+  // Apparently, the concept CGAL::Partition_traits_2 perscribes the nested
+  // type Polygon_2 to be CGAL::Polygon_2<Kernel, std::list<Point_2>>.
+  // However, we use the type CGAL::Polygon_2<Kernel, std::vector<Point_2>>,
+  // So we copy all input polygons...
+  auto sbegin = stl_input_iterator<Polygon_2>(lst);
+  auto send = stl_input_iterator<Polygon_2>(lst, false);
+  using My_polygon_2 = CGAL::Polygon_2<Kernel, std::list<Kernel::Point_2>>;
+  std::vector<My_polygon_2> pgns(lst.size());
+  std::transform(sbegin, send, pgns.begin(),
+                 [] (const Polygon_2& src) -> My_polygon_2 {
+                   My_polygon_2 trg(src.vertices_begin(), src.vertices_end());
+                   return trg;
+                 });
   auto begin = pgns.begin();
   auto end = pgns.end();
-#else
-  auto begin = stl_input_iterator<Polygon_2>(lst);
-  auto end = stl_input_iterator<Polygon_2>(lst, false);
-#endif
   return CGAL::y_monotone_partition_is_valid_2(p.vertices_begin(),
-                                               p.vertices_end(),
-                                               begin, end);
+                                               p.vertices_end(), begin, end);
 }
 
 //
@@ -113,31 +102,45 @@ bool is_convex_2(Polygon_2& p)
 
 //
 template<typename T>
-void polygon_vertical_decomposition_2(Polygon_vertical_decomposition_2& pvd,
-                                      T& polygon, py::list& res) {
-  auto v = std::vector<Polygon_2>();
-  pvd(polygon, std::back_inserter(v));
-  for (auto trapezoid : v) res.append(trapezoid);
+py::list polygon_vertical_decomposition_2(Polygon_vertical_decomposition_2& pvd,
+                                          T& polygon) {
+  py::list res;
+  auto op = [&] (const Polygon_2& pgn) mutable
+  { res.append(pgn); };
+  // The argument type of boost::function_output_iterator (UnaryFunction) must
+  // be Assignable and Copy Constructible; hence the application of std::ref().
+  auto it = boost::make_function_output_iterator(std::ref(op));
+  pvd(polygon, it);
+  return res;
 }
 
 //
-template<typename T>
-void polygon_triangulation_decomposition_2
-(Polygon_triangulation_decomposition_2& ptd, T& polygon, py::list& res)
+template <typename T>
+py::list polygon_triangulation_decomposition_2
+(Polygon_triangulation_decomposition_2& ptd, T& polygon)
 {
-  auto v = std::vector<Polygon_2>();
-  ptd(polygon, std::back_inserter(v));
-  for (auto triangle : v) res.append(triangle);
+  py::list res;
+  auto op = [&] (const Polygon_2& pgn) mutable
+  { res.append(pgn); };
+  // The argument type of boost::function_output_iterator (UnaryFunction) must
+  // be Assignable and Copy Constructible; hence the application of std::ref().
+  auto it = boost::make_function_output_iterator(std::ref(op));
+  ptd(polygon, it);
+  return res;
 }
 
 //
-void small_side_angle_bisector_decomposition_2
-(Small_side_angle_bisector_decomposition_2& ssabd,
- Polygon_2& polygon, py::list& res)
+py::list small_side_angle_bisector_decomposition_2
+(Small_side_angle_bisector_decomposition_2& ssabd, Polygon_2& polygon)
 {
-  auto v = std::vector<Polygon_2>();
-  ssabd(polygon, std::back_inserter(v));
-  for (auto c_polygon : v) res.append(c_polygon);
+  py::list res;
+  auto op = [&] (const Polygon_2& pgn) mutable
+  { res.append(pgn); };
+  // The argument type of boost::function_output_iterator (UnaryFunction) must
+  // be Assignable and Copy Constructible; hence the application of std::ref().
+  auto it = boost::make_function_output_iterator(std::ref(op));
+  ssabd(polygon, it);
+  return res;
 }
 
 }
@@ -149,15 +152,15 @@ void export_polygon_partition_2(py::module_& m) {
   using PTD_2 = pp2::Polygon_triangulation_decomposition_2;
   using SSABD_2 = pp2::Small_side_angle_bisector_decomposition_2;
 
-  m.def("approx_convex_partition_2", &pp2::approx_convex_partition_2);
-  m.def("greene_approx_convex_partition_2",
-        &pp2::greene_approx_convex_partition_2);
-  m.def("optimal_convex_partition_2", &pp2::optimal_convex_partition_2);
-  m.def("y_monotone_partition_2", &pp2::y_monotone_partition_2);
-  m.def("partition_is_valid_2", &pp2::partition_is_valid_2);
-  m.def("convex_partition_is_valid_2", &pp2::convex_partition_is_valid_2);
-  m.def("y_monotone_partition_is_valid_2",
-        &pp2::y_monotone_partition_is_valid_2);
+  // m.def("approx_convex_partition_2", &pp2::approx_convex_partition_2);
+  // m.def("greene_approx_convex_partition_2",
+  //       &pp2::greene_approx_convex_partition_2);
+  // m.def("optimal_convex_partition_2", &pp2::optimal_convex_partition_2);
+  // m.def("y_monotone_partition_2", &pp2::y_monotone_partition_2);
+  // m.def("partition_is_valid_2", &pp2::partition_is_valid_2);
+  // m.def("convex_partition_is_valid_2", &pp2::convex_partition_is_valid_2);
+  // m.def("y_monotone_partition_is_valid_2",
+  //       &pp2::y_monotone_partition_is_valid_2);
   m.def("is_y_monotone_2", &pp2::is_y_monotone_2);
   m.def("is_convex_2", &pp2::is_convex_2);
 
