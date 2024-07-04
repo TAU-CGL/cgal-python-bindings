64fc0b174f06347e98f25e345a707ede3e8c20b1
Radosław Dąbkowski <radekaadek@gmail.com>
2024-07-04 08:15:29 +0000

    run2.sh edited online with Bitbucket

--- src/scripts/run2.sh
+++ src/scripts/run2.sh
@@ -1,6 +1,16 @@
 # cmake -C ../../cmake/tests/release/aos2_epec_fixed_release.cmake ../../
 # now execute the command for all files in ../../cmake/tests/release
 
+# check if the user is in a venv
+if [ -z "$VIRTUAL_ENV" ]; then
+  echo "Please activate the virtual environment"
+  exit 1
+fi
+
+# to be sure
+python3 -m pip install build
+
+
 for file in ../../cmake/tests/release/*.cmake; do
   # make a directory for each file and enter it
   dir=$(basename $file .cmake)
@@ -9,7 +19,6 @@ for file in ../../cmake/tests/release/*.cmake; do
   # run the command
   cmake -C ../$file ../../../
   make
-  python -m pip install build
   # install eg. pip install src/libs/cgalpy/dist/CGALPY_kerEpecInt_aos2AlgPl-1.0.0-py3-none-any.whl
   pip install src/libs/cgalpy/dist/*.whl
   # leave the directory
