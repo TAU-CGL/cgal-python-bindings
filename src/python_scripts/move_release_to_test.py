53242561d01b7c45d4793181d207ca40a79d63cc
radekaadek <radekaadek@gmail.com>
2024-07-08 15:44:06 +0200

    move to test update

--- src/python_scripts/move_release_to_test.py
+++ src/python_scripts/move_release_to_test.py
@@ -12,27 +12,26 @@ def change_release(matched_name: str):
 
 def main():
     # copy all files from ../release here
-    files = os.listdir(f"{ROOT_DIR}/cmake/tests/release")
+    cmake_release_dir = f"{ROOT_DIR}/cmake/tests/release"
+    cmake_test_dir = f"{ROOT_DIR}/cmake/tests/test"
+    files = os.listdir(cmake_release_dir)
     # check if the directory exists
     if not os.path.exists(f"{ROOT_DIR}/cmake/tests/test"):
         os.makedirs(f"{ROOT_DIR}/cmake/tests/test")
     for file in files:
         if file.endswith('.cmake'):
-            # shutil.copy2(f'../release/{file}', f'./{file}')
-            shutil.copy2(f"{ROOT_DIR}/cmake/tests/release/{file}", f'{ROOT_DIR}/cmake/tests/test/{file}')
-    # get the current directory
-    current_dir = os.getcwd()
-    # get the list of all the files in the current directory
-    files = os.listdir(current_dir)
+            name = change_release(file)
+            shutil.copy2(f"{cmake_release_dir}/{file}", f'{cmake_test_dir}/{name}')
     # iterate through all the files
+    files = os.listdir(cmake_test_dir)
     for file in files:
         # check file extension .cmake
         if not file.endswith('.cmake'):
             continue
         # replace the lines with set(CGALPY_FIXED_LIBRARY_NAME OFF CACHE BOOL "" FORCE) to ON
-        with open(file, 'r') as f:
+        with open(f"{str(cmake_test_dir)}/{file}", 'r') as f:
             lines = f.readlines()
-        with open(file, 'w') as f:
+        with open(f"{str(cmake_test_dir)}/{file}", 'w') as f:
             for line in lines:
                 if 'set(CGALPY_FIXED_LIBRARY_NAME OFF CACHE BOOL "" FORCE)' in line:
                     f.write('set(CGALPY_FIXED_LIBRARY_NAME ON CACHE BOOL "" FORCE)\n')
