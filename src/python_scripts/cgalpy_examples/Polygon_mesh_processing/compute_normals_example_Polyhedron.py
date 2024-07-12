9f0af0bdefa000015ba92def8cfcc68e511355c3
radekaadek <radekaadek@gmail.com>
2024-07-12 16:41:27 +0200

    2 more examples

---
+++ src/python_scripts/cgalpy_examples/Polygon_mesh_processing/compute_normals_example_Polyhedron.py
@@ -0,0 +1,28 @@
+import os
+import sys
+import importlib
+if len(sys.argv) < 3:
+  sys.path.append(os.path.abspath('../precompiled'))
+  lib = 'CGALPY'
+else:
+  lib = sys.argv[2]
+CGALPY = importlib.import_module(lib)
+Pol3 = CGALPY.Pol3
+Pmp = CGALPY.Pmp
+
+filename = 'meshes/eight.off' if len(sys.argv) < 2 else sys.argv[1]
+
+mesh = Pol3.read_polygon_mesh(filename)
+
+normals = Pmp.compute_normals(mesh) # normals = ((face_handle, Vector), (vertex_handle, Vector))
+face_normal = normals[0]
+vertex_normal = normals[1]
+
+print("Face normals :")
+for n in face_normal[1]:
+    print(n)
+
+print("Vertex normals :")
+for n in vertex_normal[1]:
+    print(n)
+
