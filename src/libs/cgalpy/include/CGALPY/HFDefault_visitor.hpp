b5798b2c8431d291ec46d24a22322cec6a9f0510
radekaadek <radekaadek@gmail.com>
2024-07-31 15:23:15 +0200

    hfvisitor fix

--- src/libs/cgalpy/include/CGALPY/HFDefault_visitor.hpp
+++ src/libs/cgalpy/include/CGALPY/HFDefault_visitor.hpp
@@ -13,40 +13,41 @@ namespace pmp {
 
 struct HFDefault_visitor : HF::Default_visitor {
 
-  HFDefault_visitor() :
-    spp(),
-    epp(),
-    sqp(),
-    qs(),
-    ecp(),
-    scp(),
-    cs(),
-    ecup(),
-    srp(),
-    erp(),
-    sfp(),
-    efp()
-  {}
+  HFDefault_visitor() {
+    spp = nullptr;
+    epp = nullptr;
+    sqp = nullptr;
+    qs = nullptr;
+    ecp = nullptr;
+    scp = nullptr;
+    cs = nullptr;
+    ecup = nullptr;
+    srp = nullptr;
+    erp = nullptr;
+    sfp = nullptr;
+    efp = nullptr;
+  }
 
-  void start_planar_phase() const {spp();}
-  void end_planar_phase(bool success) const {epp(success); std::cerr <<"here\n";}
-  void start_quadratic_phase(std::size_t n) const {sqp(n);}
-  void quadratic_step() const {qs();}
-  void end_quadratic_phase(bool success) const {ecp(success);}
-  void start_cubic_phase(std::size_t n) const {scp(n);}
-  void cubic_step() const {cs();}
-  void end_cubic_phase() const {ecup();}
-  void start_refine_phase() const {srp();}
-  void end_refine_phase() const {erp();}
-  void start_fair_phase() const {sfp();}
-  void end_fair_phase() const {efp();}
+  void start_planar_phase() const {if (spp) spp();}
+  void end_planar_phase(bool success) const {if (epp) epp(success);}
+  void start_quadratic_phase(std::size_t n) const {if (sqp) sqp(n);}
+  void quadratic_step() const {if (qs) qs();}
+  void end_quadratic_phase(bool success) const {if (ecp) ecp(success);}
+  void start_cubic_phase(int n) const {if (scp) scp(n);}
+  void cubic_step() const {if (cs) cs();}
+  void end_cubic_phase() const {if (ecup) ecup();}
+  void start_refine_phase() const {if (srp) srp();}
+  void end_refine_phase() const {if (erp) erp();}
+  void start_fair_phase() const {if (sfp) sfp();}
+  void end_fair_phase() const {if (efp) efp();}
+
 
   void set_start_planar_phase(const std::function<void()>& fn) {spp = fn;}
-  void set_end_planar_phase(const std::function<void(bool)>& fn) {epp = fn; std::cerr <<"here2;";}
+  void set_end_planar_phase(const std::function<void(bool)>& fn) {epp = fn;}
   void set_start_quadratic_phase(const std::function<void(std::size_t)>& fn) {sqp = fn;}
   void set_quadratic_step(const std::function<void()>& fn) {qs = fn;}
   void set_end_quadratic_phase(const std::function<void(bool)>& fn) {ecp = fn;}
-  void set_start_cubic_phase(const std::function<void(std::size_t)>& fn) {scp = fn;}
+  void set_start_cubic_phase(const std::function<void(int)>& fn) {scp = fn;}
   void set_cubic_step(const std::function<void()>& fn) {cs = fn;}
   void set_end_cubic_phase(const std::function<void()>& fn) {ecup = fn;}
   void set_start_refine_phase(const std::function<void()>& fn) {srp = fn;}
@@ -60,7 +61,7 @@ private:
   std::function<void(std::size_t)> sqp;
   std::function<void()> qs;
   std::function<void(bool)> ecp;
-  std::function<void(std::size_t)> scp;
+  std::function<void(int)> scp;
   std::function<void()> cs;
   std::function<void()> ecup;
   std::function<void()> srp;
