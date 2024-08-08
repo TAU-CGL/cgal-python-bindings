e4ef2d95d562f76aa44fcd9384a663cfe0cd9fd7
radekaadek <radekaadek@gmail.com>
2024-08-08 10:52:47 +0200

    choose and compile fix

--- src/scripts/choose_and_compile.sh
+++ src/scripts/choose_and_compile.sh
@@ -6,7 +6,7 @@
 
 root_dir=${1:-../..}
 root_real=$(realpath $root_dir)
-cmakes_path=$root_real/cmake/tests/test/*.cmake
+cmakes_path=$root_real/cmake/tests/*.cmake
 
 
 # check if the user is in a virtual environment, this is useful to me
@@ -47,12 +47,43 @@ done
 cmake_file=${cmake_files[$cmake_file_number]}
 dir=$(basename $cmake_file .cmake)
 
-# Create a directory for the chosen cmake file and enter it
-mkdir -p $dir
-cd $dir
+# Ask the user if they want a fixed library name
+read -p "Do you want to use a fixed library name? (Y/n): " fixed_lib_name
+# Default is yes
+if [ -z "$fixed_lib_name" ] || [ "$fixed_lib_name" == "Y" ] || [ "$fixed_lib_name" == "y" ]; then
+  fixed_lib_name="set(CGALPY_FIXED_LIBRARY_NAME ON CACHE BOOL \"\" FORCE)"
+  lib_name="fixed"
+else
+  fixed_lib_name="set(CGALPY_FIXED_LIBRARY_NAME OFF CACHE BOOL \"\" FORCE)"
+  lib_name="not_fixed"
+fi
 
-# Run the cmake command
-if ! cmake -C $cmake_file $root_real; then
+# Ask the user for build type
+read -p "Enter the build type (Release/debug): " build_type
+# Default is Release, check if starts with R or r
+if [ -z "$build_type" ] || [[ "$build_type" == [Rr]* ]]; then
+  build_type="set(CMAKE_BUILD_TYPE \"Release\" CACHE STRING \"\" FORCE)"
+  build_str="release"
+else
+  build_type="set(CMAKE_BUILD_TYPE \"Debug\" CACHE STRING \"\" FORCE)"
+  build_str="debug"
+fi
+
+# Create a directory for the chosen cmake file and enter it
+dirname="$(basename $cmake_file .cmake)_$lib_name"
+dirname="$dirname"_"$build_str"
+mkdir -p $dirname
+cd $dirname
+
+# create the cmake file with user choices
+# Write all lines from the original cmake file to the new file
+new_filename="$(basename $cmake_file .cmake)_user_choices.cmake"
+cat $cmake_file > $new_filename
+echo $fixed_lib_name >> $new_filename
+echo $build_type >> $new_filename
+
+# Run the cmake command with the new cmake file
+if ! cmake -C $new_filename $root_real; then
   echo "CMake command failed"
   exit 1
 fi
