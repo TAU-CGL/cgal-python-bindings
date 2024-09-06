7fcdc4472723e6b27eeeff67df0ae9102190f618
radekaadek <radekaadek@gmail.com>
2024-09-06 11:33:30 +0200

    update build

--- build_command.sh
+++ build_command.sh
@@ -1,1 +1,1 @@
-CIBW_BEFORE_BUILD="pip install conan && conan profile detect && conan install . --build missing -s compiler.cppstd=gnu17 -s compiler.version=14.2" CIBW_BUILD="cp311-* cp310-* cp312-*" cibuildwheel
+cibuildwheel
