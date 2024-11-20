8be7148301645365a01247b7a64d873d7a332cbb
Efi Fogel <efifogel@gmail.com>
2024-11-20 18:33:30 +0200

    Added experimental fields

--- src/libs/cgalpy/lib/basic_viewer_bindings.cpp
+++ src/libs/cgalpy/lib/basic_viewer_bindings.cpp
@@ -8,12 +8,33 @@
 
 #include <nanobind/nanobind.h>
 
+#include <CGAL/IO/Color.h>
+
 #include "CGALPY/basic_viewer_types.hpp"
 #include "CGALPY/add_attr.hpp"
 
 namespace py = nanobind;
 
 namespace bvr {
+
+void apply_colored_face(Graphics_scene_options& gso, py::object) {
+  gso.colored_face =
+    [](const Ds&, Face_descriptor) -> bool
+    { return true; };
+}
+
+void apply_face_color(Graphics_scene_options& gso, py::object) {
+  std::size_t id(0);
+  gso.face_color =
+    [&id](const Ds& arr, Face_descriptor) ->
+    CGAL::IO::Color {
+      double h = 360 * id++ / arr.number_of_faces();
+      CGAL::IO::Color c;
+      c.set_hsv(h, 50, 50);
+      return c;
+    };
+}
+
 }
 
 void export_basic_viewer(py::module_& m) {
@@ -22,6 +43,8 @@ void export_basic_viewer(py::module_& m) {
   if (! add_attr<Gso>(m, "Graphics_scene_options")) {
     py::class_<Gso> gso_c(m, "Graphics_scene_options");
     gso_c.def(py::init<>())
+      .def("colored_face", &bvr::apply_colored_face)
+      .def("face_color", &bvr::apply_face_color)
       ;
   }
 }
