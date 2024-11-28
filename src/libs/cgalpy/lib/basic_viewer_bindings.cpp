cf82b91b8849f7a079e2de492058323f25f90c9a
Efi Fogel <efifogel@gmail.com>
2024-11-28 15:22:42 +0200

    Added explanation

--- src/libs/cgalpy/lib/basic_viewer_bindings.cpp
+++ src/libs/cgalpy/lib/basic_viewer_bindings.cpp
@@ -17,6 +17,31 @@ namespace py = nanobind;
 
 namespace bvr {
 
+/*! The implementation of the bindings of the CGAL::Graphics_scene_options class
+ * template presents a specific challenge. The interface of this class template
+ * consists of several callback functions optionally set by the user. Let `gso`
+ * be an object the type of which is an instance of the
+ * CGAL::Graphics_scene_options class template. The statement, for instance,
+ * `gso.colored_face = colored_face` sets the callback to a user provided
+ * function called `colored_face`, which determines whether to draw faces.  This
+ * function must have a specific signature, namely `bool colored_function(const
+ * Arrangement&, Face_descriptor fd)`.  Naturally, when implementing the
+ * bindings we would like to let the Python user provide a python function that
+ * determines whether to draw faces. Therefore, we create a function in C++ that
+ * simply wraps the call to the user (developer) provided Python function. We
+ * need to store both functions, namely, the Python function and the wrapper and
+ * associate them with the `gso` object.  One solution is to maintain a global
+ * mapping from CGAL::Graphics_scene_options objects to pairs of functions as
+ * described above. Maintaining static data by shared libraries is prone to
+ * errors.  Instead, we introduce a class template
+ * Graphics_scene_options_extended that derives from
+ * CGAL::Graphics_scene_options and maintains the two functions above. The
+ * Python user should only use Graphics_scene_options_extended; thus we bind
+ * this class to the attribute name "Graphics_scene_options", while the CGAL
+ * class template Graphics_scene_options is bound to the attribute name
+ * "Graphics_scene_options_base". The latter is probably of little, or no use,
+ * for a Python user.
+ */
 void export_basic_viewer(py::module_& m) {
   using Gso = bvr::Graphics_scene_options;
   if (! add_attr<Gso>(m, "Graphics_scene_options_base")) {
