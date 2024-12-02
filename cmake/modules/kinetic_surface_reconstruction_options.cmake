422d2193165203e8f3b7cf99deae30a770940c3d
Efi Fogel <efifogel@gmail.com>
2024-12-02 16:42:55 +0200

    Handled Kinetic_surface_reconstruction

--- cmake/modules/kinetic_surface_reconstruction_options.cmake
+++ cmake/modules/kinetic_surface_reconstruction_options.cmake
@@ -1,12 +1,15 @@
 if (NOT KINETIC_SURFACE_RECONSTRUCTION_OPTIONS_FILE_INCLUDED)
-set(KINETIC_SURFACE_RECONSTRUCTION_OPTIONS_FILE_INCLUDED)
+set (KINETIC_SURFACE_RECONSTRUCTION_OPTIONS_FILE_INCLUDED)
 
-function(select_kinetic_surface_reconstruction)
+function (select_kinetic_surface_reconstruction)
   if (CGALPY_KINETIC_SURFACE_RECONSTRUCTION_BINDINGS)
-    set(CGALPY_3D_POINT_SET_BINDINGS ON CACHE BOOL "" FORCE)
-    add_definitions(-DCGALPY_KINETIC_SURFACE_RECONSTRUCTION_BINDINGS=)
-  endif()
-endfunction()
+    set (CGALPY_3D_POINT_SET_BINDINGS ON CACHE BOOL "" FORCE)
+    add_definitions (-DCGALPY_KINETIC_SURFACE_RECONSTRUCTION_BINDINGS=)
+  endif ()
+endfunction ()
 
+function (kinetic_surface_reconstruction_lib_name ret)
+  set (${ret} "ksr" PARENT_SCOPE)
+endfunction ()
 
-endif()
+endif ()
