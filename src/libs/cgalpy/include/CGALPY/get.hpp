9c8420f02894923ce3afe3e42f3c33b56518ada0
Efi Fogel <efifogel@gmail.com>
2024-12-31 18:26:14 +0200

    Removed unnecessary include

--- src/libs/cgalpy/include/CGALPY/get.hpp
+++ src/libs/cgalpy/include/CGALPY/get.hpp
@@ -1,18 +1,23 @@
+// Copyright (c) 2022 Israel.
+// All rights reserved to Tel Aviv University.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later.
+// Commercial use is authorized only through a concession contract to purchase a commercial license for CGAL.
+//
+// Author(s): Efi Fogel         <efifogel@gmail.com>
+
 #ifndef CGALPY_GET_HPP
 #define CGALPY_GET_HPP
 
 #include <CGAL/boost/graph/properties.h>
-#include <CGAL/Polygon_mesh_processing/interpolated_corrected_curvatures.h>
 
 #include <nanobind/nanobind.h>
 
 namespace internal {
 
 template <typename T1, typename T2>
-auto get(const T1& p, const T2& t) {
-  return get(p, t);
-}
+auto get(const T1& p, const T2& t) { return get(p, t); }
 
 } // namespace internal
 
-#endif // CGALPY_GET_HPP
+#endif
