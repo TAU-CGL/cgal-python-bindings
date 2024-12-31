0b347d31be1baf48912927466c750ca1df1cf32a
Efi Fogel <efifogel@gmail.com>
2024-12-31 17:26:51 +0200

    1st revision

---
+++ src/libs/cgalpy/Windows/Qt/__init__.py
@@ -0,0 +1,6 @@
+import os
+os.add_dll_directory("@Boost_LIBRARY_DIRS@")
+os.add_dll_directory("@GMP_LIBRARY_DIR@")
+os.add_dll_directory("@MPFR_LIBRARY_DIR@")
+os.add_dll_directory("@Qt6_LIBRARY_DIR@")
+from .@CGALPY_LIB_NAME@ import *
