7fd70d0324fb4d621ea3d73bd33d411f1f6a8969
Efi Fogel <efifogel@gmail.com>
2025-02-10 18:23:36 +0200

    Fixed intersection

--- src/libs/cgalpy/include/CGALPY/aos_2_concepts/intersect_2_call_operator.hpp
+++ src/libs/cgalpy/include/CGALPY/aos_2_concepts/intersect_2_call_operator.hpp
@@ -17,17 +17,17 @@ namespace py = nanobind;
 
 //
 template <typename T>
-void intersect_2_call_operator(typename T::Intersect_2& i,
+py::list intersect_2_call_operator(typename T::Intersect_2& intersect,
                                typename T::X_monotone_curve_2& xc1,
-                               typename T::X_monotone_curve_2& xc2,
-                               py::list& res) {
+                               typename T::X_monotone_curve_2& xc2) {
+  py::list res;
   using Point_2 = typename T::Point_2;
   using X_monotone_curve_2 = typename T::X_monotone_curve_2;
   using Multiplicity = typename T::Multiplicity;
   using pair = std::pair<Point_2, Multiplicity>;
 
   auto v = std::vector<std::variant<pair, X_monotone_curve_2>>();
-  i(xc1, xc2, std::back_inserter(v));
+  intersect(xc1, xc2, std::back_inserter(v));
   for (auto o : v) {
     if (pair* pa = std::get_if<pair>(&o)) {
       py::tuple tup = py::make_tuple(pa->first, pa->second);
@@ -37,6 +37,7 @@ void intersect_2_call_operator(typename T::Intersect_2& i,
       res.append(*curve);
     }
   }
+  return res;
 }
 
 #endif
