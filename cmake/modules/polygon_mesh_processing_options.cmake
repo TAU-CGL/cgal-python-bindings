9451c7b2bcebb442cf9c4cfc2394a6309cbe67b6
Efi Fogel <efifogel@gmail.com>
2025-04-15 14:42:09 +0300

    Fix the mesh rep selection

--- cmake/modules/polygon_mesh_processing_options.cmake
+++ cmake/modules/polygon_mesh_processing_options.cmake
@@ -1,7 +1,29 @@
 if(NOT POLYGON_MESH_PROCESSING_OPTIONS_FILE_INCLUDED)
 set(POLYGON_MESH_PROCESSING_OPTIONS_FILE_INCLUDED)
 
+# Polygonal mesh Options
+set(CGALPY_PMP_POLYHEDRON_3_POLYGONAL_MESH      0)
+set(CGALPY_PMP_SURFACE_MESH_3_POLYGONAL_MESH    1)
+
+# Polygonal mesh Names
+set(CGALPY_PMP_POLYGONAL_MESH_SHORT_NAMES pol3 sm)
+set(CGALPY_PMP_POLYGONAL_MESH_NAMES polyhedron surfaceMesh)
+
+# Geometry Traits Default
+set(CGALPY_PMP_POLYGONAL_MESH_NAME "surfaceMesh" CACHE STRING "The polygonal mesh to use")
+set(CGALPY_PMP_POLYGONAL_MESH ${CGALPY_PMP_SURFACE_MESH_3_POLYGONAL_MESH} CACHE INTERNAL "")
+set_property(CACHE CGALPY_PMP_POLYGONAL_MESH_NAME PROPERTY STRINGS polyhedron surfaceMesh)
+
 # Selection
+function(select_polygonal_mesh)
+  if     ("${CGALPY_PMP_POLYGONAL_MESH_NAME}" STREQUAL "polyhedron")
+    set(CGALPY_PMP_POLYGONAL_MESH ${CGALPY_PMP_POLYHEDRON_3_POLYGONAL_MESH} CACHE INTERNAL "")
+  elseif ("${CGALPY_PMP_POLYGONAL_MESH_NAME}" STREQUAL "surfaceMesh")
+    set(CGALPY_PMP_POLYGONAL_MESH ${CGALPY_PMP_SURFACE_MESH_3_POLYGONAL_MESH} CACHE INTERNAL "")
+  endif()
+  add_definitions(-DCGALPY_PMP_POLYGONAL_MESH=${CGALPY_PMP_POLYGONAL_MESH})
+endfunction()
+
 # 1. combinatorial repair
 function(select_pmp_combinatorial_repair)
   if(${CGALPY_PMP_COMBINATORIAL_REPAIR_BINDINGS})
@@ -163,7 +185,7 @@ function(get_polygon_mesh_processing_lib_name ret)
     set (part13 "Mes")
   endif ()
   if (${CGALPY_PMP_NORMAL_COMPUTATION_BINDINGS})
-    set (part15 "Nc")
+    set (part14 "Nc")
   endif ()
   if (${CGALPY_PMP_ORIENTATION_BINDINGS})
     set (part15 "Ori")
