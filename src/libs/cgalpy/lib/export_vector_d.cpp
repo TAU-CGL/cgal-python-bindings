a290908cc8d0b72cfb2fce98a8767f0eb43c3fb6
Efi Fogel <efifogel@gmail.com>
2025-06-28 11:41:37 +0300

    Enhanced

--- src/libs/cgalpy/lib/export_vector_d.cpp
+++ src/libs/cgalpy/lib/export_vector_d.cpp
@@ -9,7 +9,21 @@
 #include <nanobind/nanobind.h>
 #include <nanobind/operators.h>
 
+#include "CGALPY/add_insertion.hpp"
+#include "CGALPY/Hash_rational_point.hpp"
 #include "CGALPY/kernel_d_types.hpp"
+#include "CGALPY/make_iterator.hpp"
+#include "CGALPY/stl_input_iterator.hpp"
+
+namespace kerd {
+
+//!
+void init_vector_d(Vector_d* pd, int d, py::list& lst) {
+  auto begin = stl_input_iterator<FT_d>(lst);
+  auto end = stl_input_iterator<FT_d>(lst, false);
+}
+
+}
 
 //!
 void export_vector_d(py::class_<Vector_d>& vecd_c) {
@@ -17,6 +31,28 @@ void export_vector_d(py::class_<Vector_d>& vecd_c) {
   using Vecd = Vector_d;
 
   vecd_c.def(py::init<>())
+    .def("__init__", &kerd::init_vector_d)
+    .def("__hash__", &hash_rational_point<is_epec_d_type(), Vecd>)
+    .def("dimension", &Vecd::dimension)
+    .def("cartesian", &Vecd::cartesian)
     .def("__getitem__", &Vecd::operator[])
+    .def(py::self == py::self)
+    .def(py::self != py::self)
+#if (CGALPY_KERNEL_D != CGALPY_KERNEL_D_EPEC_D)
+    .def(py::self > py::self)
+    .def(py::self < py::self)
+    .def(py::self <= py::self)
+    .def(py::self >= py::self)
+#endif
     ;
+
+  using Cci = Kerd::Cartesian_const_iterator_d;
+  add_iterator<Cci, Cci, const FT_d&>("Cartesian_iterator", vecd_c);
+  vecd_c.def("cartesians",
+             [](const Vecd& v)
+             { return make_iterator(v.cartesian_begin(), v.cartesian_end()); },
+             py::keep_alive<0, 1>());
+
+  add_insertion(vecd_c, "__str__");
+  add_insertion(vecd_c, "__repr__");
 }
