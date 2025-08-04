6c4634465b8c40384627c2f7329818ca613d765d
Efi Fogel <efifogel@gmail.com>
2025-08-04 11:20:41 +0300

    Resurected

--- src/libs/cgalpy/include/CGALPY/export_property_map.hpp
+++ src/libs/cgalpy/include/CGALPY/export_property_map.hpp
@@ -1,5 +1,13 @@
-#ifndef CGALPY_PROPERTY_MAP_HPP
-#define CGALPY_PROPERTY_MAP_HPP
+// Copyright (c) 2022 Israel.
+// All rights reserved to Tel Aviv University.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later.
+// Commercial use is authorized only through a concession contract to purchase a commercial license for CGAL.
+//
+// Author(s): Efi Fogel         <efifogel@gmail.com>
+
+#ifndef CGALPY_EXPOST_PROPERTY_MAP_HPP
+#define CGALPY_EXPOST_PROPERTY_MAP_HPP
 
 #include <nanobind/nanobind.h>
 #include <nanobind/make_iterator.h>
@@ -10,6 +18,7 @@ namespace py = nanobind;
 
 namespace internal {
 
+//!
 template <typename Mesh, typename Key, typename Value>
 auto export_property_map(py::module_& m, const std::string& name) {
   using Mesh_property_map = typename Mesh::template Property_map<Key, Value>;
@@ -21,57 +30,42 @@ auto export_property_map(py::module_& m, const std::string& name) {
   // return for extending with .def()
   return py::class_<Mesh_property_map>(m, name.c_str())
     .def(py::init<>())
-    .def("__getitem__", [](Mesh_property_map& self, Key key) -> Value {
-      return self[key];
-    })
-    .def("__setitem__", [](Mesh_property_map& self, Key key, Value value) {
-      self[key] = value;
-    })
+    .def("__getitem__", [](Mesh_property_map& self, Key key) -> Value { return self[key]; })
+    .def("__setitem__", [](Mesh_property_map& self, Key key, Value value) { self[key] = value; })
     .def("__iter__", [](Mesh_property_map& self) {
-      return py::make_iterator(py::type<typename Mesh_property_map::iterator>(), "Iterator", self.begin(), self.end(), py::keep_alive<0, 1>());
+      return py::make_iterator(py::type<typename Mesh_property_map::iterator>(),
+                               "Iterator", self.begin(), self.end(), py::keep_alive<0, 1>());
     })
-    .def("transfer", [](Mesh_property_map& self, const Mesh_property_map& other) {
-      return self.transfer(other);
-    })
-    .def("transfer", [](Mesh_property_map& self, const Mesh_property_map& other, std::size_t from, std::size_t to) {
-      return self.transfer(other, from, to);
-    })
+    .def("transfer", [](Mesh_property_map& self, const Mesh_property_map& other) { return self.transfer(other); })
+    .def("transfer", [](Mesh_property_map& self, const Mesh_property_map& other, std::size_t from, std::size_t to)
+    { return self.transfer(other, from, to); })
     .def("reset", &Mesh_property_map::reset)
     ;
 }
 
+//!
 template <typename Mesh, typename Key>
 auto export_property_map_bool(py::module_& m, const std::string& name) {
   using Mesh_property_map = typename Mesh::template Property_map<Key, bool>;
 
-  m.def("get", [](const Mesh_property_map& p, const Key& t) {
-    return py::bool_(get(p, t));
-  });
+  m.def("get", [](const Mesh_property_map& p, const Key& t) { return py::bool_(get(p, t)); });
 
   // return for extending with .def()
   return py::class_<Mesh_property_map>(m, name.c_str())
     .def(py::init<>())
-    .def("__getitem__", [](Mesh_property_map& self, Key key) -> bool {
-      return self[key];
-    })
-    .def("__setitem__", [](Mesh_property_map& self, Key key, bool value) {
-      self[key] = value;
-    })
+    .def("__getitem__", [](Mesh_property_map& self, Key key) -> bool { return self[key]; })
+    .def("__setitem__", [](Mesh_property_map& self, Key key, bool value) { self[key] = value; })
     .def("__iter__", [](Mesh_property_map& self) { // nanobind should be able to handle std::_Bit_reference
-      return py::make_iterator(py::type<typename Mesh_property_map::iterator>(), "Iterator", self.begin(), self.end(), py::keep_alive<0, 1>());
+      return py::make_iterator(py::type<typename Mesh_property_map::iterator>(),
+                               "Iterator", self.begin(), self.end(), py::keep_alive<0, 1>());
     })
-    .def("transfer", [](Mesh_property_map& self, const Mesh_property_map& other) {
-      return self.transfer(other);
-    })
-    .def("transfer", [](Mesh_property_map& self, const Mesh_property_map& other, std::size_t from, std::size_t to) {
-      return self.transfer(other, from, to);
-    })
+    .def("transfer", [](Mesh_property_map& self, const Mesh_property_map& other) { return self.transfer(other); })
+    .def("transfer", [](Mesh_property_map& self, const Mesh_property_map& other, std::size_t from, std::size_t to)
+    { return self.transfer(other, from, to); })
     .def("reset", &Mesh_property_map::reset)
     ;
 }
 
-
-
 } // namespace internal
 
-#endif // CGALPY_PROPERTY_MAP_HPP
+#endif
