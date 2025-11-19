418515b5e4a4f24277adce7849193a63c8798773
Efi Fogel <efifogel@gmail.com>
2025-11-19 14:33:13 +0200

    Enhanced wrapping of mpq_rational

--- src/libs/cgalpy/include/CGALPY/Kernel/export_mpq_rational.hpp
+++ src/libs/cgalpy/include/CGALPY/Kernel/export_mpq_rational.hpp
@@ -6,9 +6,13 @@
 //
 // Author(s): Efi Fogel         <efifogel@gmail.com>
 
+#include <string>
+
 #include <boost/multiprecision/gmp.hpp>
 
 #include <nanobind/nanobind.h>
+#include <nanobind/operators.h>
+#include <nanobind/stl/string.h>
 
 #include "CGALPY/add_attr.hpp"
 #include "CGALPY/Kernel/export_mpz_int.hpp"
@@ -20,18 +24,19 @@ void export_mpq_rational(PyClass& cls) {
   export_mpz_int(cls);
 
   using mpz_int = boost::multiprecision::mpz_int;
-  using mpq_rational = boost::multiprecision::mpq_rational;
+  using mpq_rat = boost::multiprecision::mpq_rational;
 
-  if (! add_attr<mpq_rational>(cls, "mpq_rational")) {
-    py::class_<mpq_rational> mpq_rational_c(cls, "mpq_rational");
-    mpq_rational_c.def(py::init<const mpq_rational&>())
+  if (! add_attr<mpq_rat>(cls, "mpq_rational")) {
+    py::class_<mpq_rat> mpq_rat_c(cls, "mpq_rational");
+    mpq_rat_c.def(py::init<const mpq_rat&>())
       .def(py::init_implicit<mpz_int>())
       .def(py::init<const mpz_int&, const mpz_int&>())
       .def(py::init_implicit<double>())
-      .def(py::self == py::self,
-           py::sig("def __eq__(self, arg: object, /) -> bool"))
-      .def(py::self != py::self,
-           py::sig("def __ne__(self, arg: object, /) -> bool"))
+      .def(py::init<const std::string&>())
+      .def("numerator", [](const mpq_rat& r) { return boost::multiprecision::numerator(r); })
+      .def("denominator", [](const mpq_rat& r) { return boost::multiprecision::denominator(r); })
+      .def(py::self == py::self, py::sig("def __eq__(self, arg: object, /) -> bool"))
+      .def(py::self != py::self, py::sig("def __ne__(self, arg: object, /) -> bool"))
       .def(py::self < py::self)
       .def(py::self > py::self)
       .def(py::self <= py::self)
@@ -59,7 +64,7 @@ void export_mpq_rational(PyClass& cls) {
       .def_cast(py::self - float())
       ;
 
-    add_insertion(mpq_rational_c, "__str__");
-    add_insertion(mpq_rational_c, "__repr__");
+    add_insertion(mpq_rat_c, "__str__");
+    add_insertion(mpq_rat_c, "__repr__");
   }
 }
