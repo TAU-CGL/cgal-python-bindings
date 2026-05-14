0c15fe81b73e177c8263571bbdc61a87af77461d
Utkarsh Khajuria <utkarshkhajuria7@gmail.com>
2026-05-14 18:38:30 +0530

    Add generated docstrings to Polygon_mesh_processing Polyhedral_envelope bindings

--- src/libs/cgalpy/lib/polygon_mesh_processing_bindings.cpp
+++ src/libs/cgalpy/lib/polygon_mesh_processing_bindings.cpp
@@ -46,6 +46,7 @@
 #include "CGALPY/pmp_np_parser.hpp"
 #include "CGALPY/parse_named_parameters.hpp"
 #include "CGALPY/Polyhedral_envelope.hpp"
+#include "cgalpy/Polygon_mesh_processing_docstrings.hpp"
 #include "CGALPY/pmp_helpers.hpp"
 #include "CGALPY/Internal_face_plane_3_map.hpp"
 #include "CGALPY/merge_coplanar_facets.hpp"
@@ -366,6 +367,7 @@ auto is_needle_triangle_face(typename boost::graph_traits<TriangleMesh>::face_de
 
 // Export Polygon_mesh_processing
 void export_polygon_mesh_processing(py::module_& m) {
+  namespace doc = cgalpy::docstrings::Polygon_mesh_processing;
   using Pm = pmp::Polygonal_mesh;
   // using Vid = pmp::Vertex_identifier;
   // using Hid = pmp::Halfedge_identifier;
@@ -502,21 +504,29 @@ void export_polygon_mesh_processing(py::module_& m) {
   using Pe = pmp::Polyhedral_envelope<Pm, Kernel>;
   py::class_<Pe>(m, "Polyhedral_envelope")
     .def(py::init<Pm&, double, const py::dict&>(),
-         py::arg("tmesh"), py::arg("epsilon"), py::arg("np") = py::dict())
+         py::arg("tmesh"), py::arg("epsilon"), py::arg("np") = py::dict(),
+         doc::CGAL_Polyhedral_envelope_Polyhedral_envelope_1)
     .def(py::init<const std::vector<Fd>&, Pm&, double, const py::dict&>(),
-         py::arg("face_range"), py::arg("tmesh"), py::arg("epsilon"), py::arg("np") = py::dict())
+         py::arg("face_range"), py::arg("tmesh"), py::arg("epsilon"), py::arg("np") = py::dict(),
+         doc::CGAL_Polyhedral_envelope_Polyhedral_envelope_2)
     .def(py::init<const pmp::Point_3_vec&, const std::vector<pmp::Size_t_vec>&, double, const py::dict&>(),
          py::arg("points"), py::arg("polygons"), py::arg("epsilon"),
-         py::arg("np") = py::dict()) // TODO: handle face_epsilon_map
-    .def("is_empty", &Pe::is_empty)
-    .def("inside", [](const Pe& i, const Point_3& query) { return i(query); }, py::arg("query"))
+         py::arg("np") = py::dict(), // TODO: handle face_epsilon_map
+         doc::CGAL_Polyhedral_envelope_Polyhedral_envelope_3)
+    .def("is_empty", &Pe::is_empty,
+         doc::CGAL_Polyhedral_envelope_is_empty)
+    .def("inside", [](const Pe& i, const Point_3& query) { return i(query); }, py::arg("query"),
+         doc::CGAL_Polyhedral_envelope_operator)
     .def("inside", [](const Pe& i, const Point_3& source, const Point_3& target) { return i(source, target); },
-         py::arg("source"), py::arg("target"))
+         py::arg("source"), py::arg("target"),
+         doc::CGAL_Polyhedral_envelope_operator_1)
     .def("inside", [](const Pe& i, const Point_3& t0, const Point_3& t1, const Point_3& t2) { return i(t0, t1, t2); },
-         py::arg("t0"), py::arg("t1"), py::arg("t2"))
+         py::arg("t0"), py::arg("t1"), py::arg("t2"),
+         doc::CGAL_Polyhedral_envelope_operator_2)
     .def("inside",
          [](const Pe& i, const Pm& tmesh, const py::dict& np) { return i(tmesh, internal::parse_pmp_np<Pm>(np)); },
-         py::arg("tmesh"), py::arg("np") = py::dict())
+         py::arg("tmesh"), py::arg("np") = py::dict(),
+         doc::CGAL_Polyhedral_envelope_operator_3)
     // TODO: inside triangle range
     ;
 
