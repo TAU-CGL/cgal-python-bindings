1b4d760885f5c1d431e492345c7fbee052992e9e
Utkarsh Khajuria <utkarshkhajuria7@gmail.com>
2026-05-20 19:34:38 +0530

    AOS2: add generated docstrings and py::arg names to all traits concept headers and export files
    - Wire cgalpy::docstrings::Arrangement_on_surface_2 symbols into all 9 aos_2_concepts headers
    - Add py::arg() names to constructors and operator methods in concept headers
    - Wire generated docstrings into all 14 export_arr_*_traits_2.cpp files
    - Add py::arg() names to trait-specific .def() calls
    - Build-tested: segment variant compiles cleanly

--- src/libs/cgalpy/include/CGALPY/aos_2_concepts/export_AosSphericalBoundaryTraits_2.hpp
+++ src/libs/cgalpy/include/CGALPY/aos_2_concepts/export_AosSphericalBoundaryTraits_2.hpp
@@ -11,6 +11,7 @@
 #include <nanobind/nanobind.h>
 
 #include "CGALPY/aos_2_concepts/export_AosBasicTraits_2.hpp"
+#include "cgalpy/Arrangement_on_surface_2_docstrings.hpp"
 
 namespace py = nanobind;
 
@@ -30,7 +31,8 @@ template<typename T, typename C, typename Classes>
 void export_Compare_x_near_boundary_2(C& c, Classes& classes, CGAL::Tag_true) {
   using Cmp_x_near_bnd = typename T::Compare_x_near_boundary_2;
   classes.m_compare_x_near_boundary_2 =
-    new py::class_<Cmp_x_near_bnd>(c, "Compare_x_near_boundary_2");
+    new py::class_<Cmp_x_near_bnd>(c, "Compare_x_near_boundary_2",
+                                   doc::AosSphericalBoundaryTraits_2_Compare_x_near_boundary_2);
   classes.m_compare_x_near_boundary_2->def("__call__",
                                            &Cmp_x_near_bnd::operator());
   c.def("compare_x_near_boundary_2_object",
@@ -48,7 +50,8 @@ void export_Compare_x_on_boundary_2(C& c, Classes& classes, CGAL::Tag_true) {
   using Cmp_x_on_bnd = typename T::Compare_x_on_boundary_2;
 
   classes.m_compare_x_on_boundary_2 =
-    new py::class_<Cmp_x_on_bnd>(c, "Compare_x_on_boundary_2");
+    new py::class_<Cmp_x_on_bnd>(c, "Compare_x_on_boundary_2",
+                                 doc::AosSphericalBoundaryTraits_2_Compare_x_on_boundary_2);
   using overload1 = CGAL::Comparison_result(Cmp_x_on_bnd::*)
     (const Xcv&, CGAL::Arr_curve_end, const Xcv&, CGAL::Arr_curve_end) const;
   using overload2 = CGAL::Comparison_result (Cmp_x_on_bnd::*)
@@ -72,9 +75,11 @@ void export_Compare_y_near_boundary_2(C& c, Classes& classes, CGAL::Tag_true) {
   using Cmp_y_near_bnd = typename T::Compare_y_near_boundary_2;
 
   classes.m_compare_y_near_boundary_2 =
-    new py::class_<Cmp_y_near_bnd>(c, "Compare_y_near_boundary_2");
+    new py::class_<Cmp_y_near_bnd>(c, "Compare_y_near_boundary_2",
+                                   doc::AosSphericalBoundaryTraits_2_Compare_y_near_boundary_2);
   auto cmp_y_near_bnd_c = *(classes.m_compare_y_near_boundary_2);
-  cmp_y_near_bnd_c.def("__call__", &Cmp_y_near_bnd::operator());
+  cmp_y_near_bnd_c.def("__call__", &Cmp_y_near_bnd::operator(),
+                         doc::AosTraits_CompareYNearBoundary_2_operator);
   c.def("compare_y_near_boundary_2_object",
         &T::compare_y_near_boundary_2_object);
 }
@@ -89,8 +94,10 @@ void export_Parameter_space_in_x_2(C& c, Classes& classes, CGAL::Tag_true) {
   using overload = CGAL::Arr_parameter_space (T::Parameter_space_in_x_2::*)
     (const typename T::X_monotone_curve_2&, CGAL::Arr_curve_end) const;
   classes.m_parameter_space_in_x_2 =
-    new py::class_<typename T::Parameter_space_in_x_2>(c, "Parameter_space_in_x_2");
-  classes.m_parameter_space_in_x_2->def("__call__", static_cast<overload>(&T::Parameter_space_in_x_2::operator()));
+    new py::class_<typename T::Parameter_space_in_x_2>(c, "Parameter_space_in_x_2",
+                                                     doc::AosSphericalBoundaryTraits_2_Parameter_space_in_x_2);
+  classes.m_parameter_space_in_x_2->def("__call__", static_cast<overload>(&T::Parameter_space_in_x_2::operator()),
+                                        doc::AosTraits_ParameterSpaceInX_2_operator);
   c.def("parameter_space_in_x_2_object", &T::parameter_space_in_x_2_object);
 }
 
@@ -106,8 +113,10 @@ void export_Parameter_space_in_y_2(C& c, Classes& classes, CGAL::Tag_true) {
   using Prm_space_in_y_2 = typename T::Parameter_space_in_y_2;
 
   classes.m_parameter_space_in_y_2 =
-    new py::class_<Prm_space_in_y_2>(c, "Parameter_space_in_y_2");
-  classes.m_parameter_space_in_y_2->def("__call__", static_cast<overload>(&Prm_space_in_y_2::operator()));
+    new py::class_<Prm_space_in_y_2>(c, "Parameter_space_in_y_2",
+                                      doc::AosSphericalBoundaryTraits_2_Parameter_space_in_y_2);
+  classes.m_parameter_space_in_y_2->def("__call__", static_cast<overload>(&Prm_space_in_y_2::operator()),
+                                        doc::AosTraits_ParameterSpaceInY_2_operator);
   c.def("parameter_space_in_y_2_object", &T::parameter_space_in_y_2_object);
 }
 
@@ -123,7 +132,8 @@ void export_Is_on_y_identification_2(C& c, Classes& classes, CGAL::Tag_true) {
   using Is_on_y_iden = typename T::Is_on_y_identification_2;
 
   classes.m_is_on_y_identification_2 =
-    new py::class_<Is_on_y_iden>(c, "Is_on_y_identification_2");
+    new py::class_<Is_on_y_iden>(c, "Is_on_y_identification_2",
+                                 doc::AosSphericalBoundaryTraits_2_Is_on_y_identification_2);
   using overload2 = bool(Is_on_y_iden::*)(const Pnt&) const;
   using overload1 = bool(Is_on_y_iden::*)(const Xcv&) const;
   auto is_on_y_iden_c = *(classes.m_is_on_y_identification_2);
@@ -147,6 +157,7 @@ void export_AosSphericalBoundaryTraits_2 (C& c, Concepts& concepts) {
   using Rs_cat = Right_side_category;
   using Lr_approach_side = typename approach_side<Ls_cat, Rs_cat>::type;
 
+  namespace doc = cgalpy::docstrings::Arrangement_on_surface_2;
   static bool exported = false;
   if (exported) return;
 
