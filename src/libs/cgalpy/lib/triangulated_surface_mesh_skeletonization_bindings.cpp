793fdbb1016287b9f09e452bbd0518b5f3ca0fa1
Utkarsh Khajuria <utkarshkhajuria7@gmail.com>
2026-05-30 16:01:20 +0530

    Complete Smsk docstrings and named arguments

--- src/libs/cgalpy/lib/triangulated_surface_mesh_skeletonization_bindings.cpp
+++ src/libs/cgalpy/lib/triangulated_surface_mesh_skeletonization_bindings.cpp
@@ -18,8 +18,10 @@
 
 #include "CGALPY/polygon_mesh_processing_types.hpp"
 #include "CGALPY/Polyline_visitor.hpp"
+#include "cgalpy/Smsk_docstrings.hpp"
 
 namespace py = nanobind;
+namespace smsk_doc = cgalpy::smsk::docstrings;
 
 void export_triangulated_surface_mesh_skeletonization(py::module_& m) {
   using Tm = cgalpy::pmp::Polygonal_mesh;
@@ -36,70 +38,52 @@ void export_triangulated_surface_mesh_skeletonization(py::module_& m) {
   using Skeleton_vertex = Skeleton::vertex_descriptor;
   constexpr auto ri(py::rv_policy::reference_internal);
 
-  py::class_<Mean_curvature_flow_skeletonization> skeletonization(m, "Mean_curvature_flow_skeletonization");
+  py::class_<Mean_curvature_flow_skeletonization> skeletonization(
+      m, "Mean_curvature_flow_skeletonization",
+      smsk_doc::Mean_curvature_flow_skeletonization_class);
   skeletonization
     // Constructor
     .def(py::init<const Tm&>(), // TODO: add vertex_point_map
          py::arg("tmesh"),
-         "The constructor of a skeletonization object.\n"
-         "The algorithm parameters are initialized such that:\n"
-         "• max_triangle_angle() == 110\n"
-         "• quality_speed_tradeoff() == 0.1\n"
-         "• medially_centered_speed_tradeoff() == 0.2\n"
-         "• area_variation_factor() == 0.0001\n"
-         "• max_iterations() == 500\n"
-         "• is_medially_centered() == true\n"
-         "• min_edge_length() == 0.002 * the length of the diagonal of the bounding box of tmesh\n"
-         "\n"
-         "Precondition\n"
-         "• tmesh is a triangulated surface mesh without borders and has exactly one connected component.\n"
-         "\n"
-         "Parameters\n"
-         "• tmesh:	input triangulated surface mesh.\n")
+         smsk_doc::Mean_curvature_flow_skeletonization_Mean_curvature_flow_skeletonization)
     // Local Remeshing Parameters
     .def("max_triangle_angle", &CGAL::Mean_curvature_flow_skeletonization<Tm>::max_triangle_angle,
-         "During the local remeshing step, a triangle will be split if it has an angle larger than max_triangle_angle().\n")
+         smsk_doc::Mean_curvature_flow_skeletonization_max_triangle_angle)
     .def("min_edge_length", &CGAL::Mean_curvature_flow_skeletonization<Tm>::min_edge_length,
-         "During the local remeshing step, an edge will be collapse if it is length is less than min_edge_length().\n")
+         smsk_doc::Mean_curvature_flow_skeletonization_min_edge_length)
     .def("set_max_triangle_angle", &CGAL::Mean_curvature_flow_skeletonization<Tm>::set_max_triangle_angle,
          py::arg("value"),
-         "set function for max_triangle_angle()\n")
+         smsk_doc::Mean_curvature_flow_skeletonization_set_max_triangle_angle)
     .def("set_min_edge_length", &CGAL::Mean_curvature_flow_skeletonization<Tm>::set_min_edge_length,
          py::arg("value"),
-         "set function for min_edge_length()\n")
+         smsk_doc::Mean_curvature_flow_skeletonization_set_min_edge_length)
     // Algorithm Termination Parameters
     .def("max_iterations", &CGAL::Mean_curvature_flow_skeletonization<Tm>::max_iterations,
-         "Maximum number of iterations performed by contract_until_convergence().\n")
+         smsk_doc::Mean_curvature_flow_skeletonization_max_iterations)
     .def("area_variation_factor", &CGAL::Mean_curvature_flow_skeletonization<Tm>::area_variation_factor,
-         "The convergence is considered to be reached if the variation of the area of the meso-skeleton after one iteration is smaller than area_variation_factor()*original_area where original_area is the area of the input triangle mesh.\n")
+         smsk_doc::Mean_curvature_flow_skeletonization_area_variation_factor)
     .def("set_max_iterations", &CGAL::Mean_curvature_flow_skeletonization<Tm>::set_max_iterations,
          py::arg("value"),
-         "set function for max_iterations()\n")
+         smsk_doc::Mean_curvature_flow_skeletonization_set_max_iterations)
     .def("set_area_variation_factor", &CGAL::Mean_curvature_flow_skeletonization<Tm>::set_area_variation_factor,
          py::arg("value"),
-         "set function for area_variation_factor()\n")
+         smsk_doc::Mean_curvature_flow_skeletonization_set_area_variation_factor)
     // Vertex Motion Parameters
     .def("quality_speed_tradeoff", &CGAL::Mean_curvature_flow_skeletonization<Tm>::quality_speed_tradeoff,
-         "This is an advanced function.\n"
-         "Advanced\n"
-         "\n"
-         "Controls the velocity of movement and approximation quality: decreasing this value makes the mean curvature flow based contraction converge faster, but results in a skeleton of lower quality. This parameter corresponds to wH in the original publication.\n")
+         smsk_doc::Mean_curvature_flow_skeletonization_quality_speed_tradeoff)
     .def("is_medially_centered", &CGAL::Mean_curvature_flow_skeletonization<Tm>::is_medially_centered,
-         "If true, the meso-skeleton placement will be attracted by an approximation of the medial axis of the mesh during the contraction steps, so will be the result skeleton.\n")
+         smsk_doc::Mean_curvature_flow_skeletonization_is_medially_centered)
     .def("medially_centered_speed_tradeoff", &CGAL::Mean_curvature_flow_skeletonization<Tm>::medially_centered_speed_tradeoff,
-         "This is an advanced function.\n"
-         "Advanced\n"
-         "\n"
-         "Controls the smoothness of the medial approximation: increasing this value results in a (less smooth) skeleton closer to the medial axis, as well as a lower convergence speed. It is only used if is_medially_centered()==true. This parameter corresponds to wM in the original publication.\n")
+         smsk_doc::Mean_curvature_flow_skeletonization_medially_centered_speed_tradeoff)
     .def("set_quality_speed_tradeoff", &CGAL::Mean_curvature_flow_skeletonization<Tm>::set_quality_speed_tradeoff,
          py::arg("value"),
-         "set function for quality_speed_tradeoff()\n")
+         smsk_doc::Mean_curvature_flow_skeletonization_set_quality_speed_tradeoff)
     .def("set_is_medially_centered", &CGAL::Mean_curvature_flow_skeletonization<Tm>::set_is_medially_centered,
          py::arg("value"),
-         "set function for is_medially_centered()\n")
+         smsk_doc::Mean_curvature_flow_skeletonization_set_is_medially_centered)
     .def("set_medially_centered_speed_tradeoff", &CGAL::Mean_curvature_flow_skeletonization<Tm>::set_medially_centered_speed_tradeoff,
          py::arg("value"),
-         "set function for medially_centered_speed_tradeoff()\n")
+         smsk_doc::Mean_curvature_flow_skeletonization_set_medially_centered_speed_tradeoff)
     // TODO: what is the type of VertexIterator?
     // .def("set_fixed_vertices", &CGAL::Mean_curvature_flow_skeletonization<Tm>::set_fixed_vertices<VertexIterator>,
     //      py::arg("begin"), py::arg("end"),
@@ -110,34 +94,27 @@ void export_triangulated_surface_mesh_skeletonization(py::module_& m) {
     //      "\n");
     // High Level Functions
     .def("__call__", &CGAL::Mean_curvature_flow_skeletonization<Tm>::operator(),
-         "Creates the curve skeleton: the input surface mesh is iteratively contracted until convergence, and then turned into a curve skeleton.\n"
-         "\n"
-         "This is equivalent to calling contract_until_convergence() and convert_to_skeleton().\n"
-         "\n"
-         "Parameters\n"
-         "• skeleton: graph that will contain the skeleton of the input triangulated surface mesh. For each vertex descriptor vd of skeleton, the corresponding point and the set of input vertices that contracted to vd can be retrieved using skeleton[vd].point and skeleton[vd].vertices respectively.\n")
+         py::arg("skeleton"),
+         smsk_doc::Mean_curvature_flow_skeletonization_operator_call)
     // Low Level Functions
     .def("contract_geometry", &CGAL::Mean_curvature_flow_skeletonization<Tm>::contract_geometry,
-         "Runs one contraction step following the mean curvature flow.\n")
+         smsk_doc::Mean_curvature_flow_skeletonization_contract_geometry)
     .def("collapse_edges", &CGAL::Mean_curvature_flow_skeletonization<Tm>::collapse_edges,
-         "Collapses edges of the meso-skeleton with length less than min_edge_length() and returns the number of edges collapsed.\n")
+         smsk_doc::Mean_curvature_flow_skeletonization_collapse_edges)
     .def("split_faces", &CGAL::Mean_curvature_flow_skeletonization<Tm>::split_faces,
-         "Splits faces of the meso-skeleton having one angle greater than max_triangle_angle() and returns the number of faces split.\n")
+         smsk_doc::Mean_curvature_flow_skeletonization_split_faces)
     .def("detect_degeneracies", &CGAL::Mean_curvature_flow_skeletonization<Tm>::detect_degeneracies,
-         "Prevents degenerate vertices to move during the following contraction steps and returns the number of newly fixed vertices.\n")
+         smsk_doc::Mean_curvature_flow_skeletonization_detect_degeneracies)
     .def("contract", &CGAL::Mean_curvature_flow_skeletonization<Tm>::contract,
-         "Performs subsequent calls to contract_geometry(), collapse_edges(), split_faces() and detect_degeneracies()\n")
+         smsk_doc::Mean_curvature_flow_skeletonization_contract)
     .def("contract_until_convergence", &CGAL::Mean_curvature_flow_skeletonization<Tm>::contract_until_convergence,
-         "Iteratively calls contract() until the change of surface area of the meso-skeleton after one iteration is smaller than area_variation_factor()*original_area where original_area is the area of the input triangle mesh, or if the maximum number of iterations has been reached.\n")
+         smsk_doc::Mean_curvature_flow_skeletonization_contract_until_convergence)
     .def("convert_to_skeleton", &CGAL::Mean_curvature_flow_skeletonization<Tm>::convert_to_skeleton,
          py::arg("skeleton"),
-         "Converts the contracted surface mesh to a skeleton curve.\n"
-         "\n"
-         "Parameters\n"
-         "• skeleton	graph that will contain the skeleton of tmesh. It should be empty before passed to the function.\n")
+         smsk_doc::Mean_curvature_flow_skeletonization_convert_to_skeleton)
     // Access to the Meso-Skeleton
     .def("meso_skeleton", &CGAL::Mean_curvature_flow_skeletonization<Tm>::meso_skeleton,
-         "Access to the collapsed triangulated surface mesh.\n")
+         smsk_doc::Mean_curvature_flow_skeletonization_meso_skeleton)
     ;
 
   py::class_<Mcfs_vmap> vmap(skeletonization, "Vmap");
@@ -148,7 +125,9 @@ void export_triangulated_surface_mesh_skeletonization(py::module_& m) {
     ;
 
   // TODO: this is an unspecified_type
-  py::class_<Meso_skeleton> meso_skeleton(skeletonization, "Meso_skeleton");
+  py::class_<Meso_skeleton> meso_skeleton(
+      skeletonization, "Meso_skeleton",
+      smsk_doc::Mean_curvature_flow_skeletonization_Meso_skeleton);
 
   py::class_<boost::list_edge<unsigned long, boost::no_property>>
       boost_list_edge(skeletonization, "Boost_list_edge");
@@ -166,7 +145,9 @@ void export_triangulated_surface_mesh_skeletonization(py::module_& m) {
     // .def_ro("m_vertices", &Skeleton::stored_vertex::m_out_edges)
     ;
 
-  py::class_<Skeleton> skeleton(skeletonization, "Skeleton");
+  py::class_<Skeleton> skeleton(
+      skeletonization, "Skeleton",
+      smsk_doc::Mean_curvature_flow_skeletonization_Skeleton);
   skeleton
     .def(py::init<>(),
          "Default constructor. Creates an empty graph object with zero vertices and zero edges.\n")
@@ -196,13 +177,21 @@ void export_triangulated_surface_mesh_skeletonization(py::module_& m) {
         //                          pair.first, pair.second, py::keep_alive<0, 1>());
       // }, "Return an iterator of vertices of the skeleton.");
   m.def("num_vertices", [](const Skeleton& skeleton) { return boost::num_vertices(skeleton); },
-               "Returns the number of vertices in the skeleton.");
+        py::arg("skeleton"),
+        "Returns the number of vertices in the skeleton.");
   m.def("num_edges", [](const Skeleton& skeleton) { return boost::num_edges(skeleton); },
-               "Returns the number of edges in the skeleton.");
-  m.def("source", [](const boost::list_edge<unsigned long, boost::no_property>& e, const Skeleton& skeleton) { source(e, skeleton); },
-               "Returns the source vertex of the edge e.");
-  m.def("target", [](const boost::list_edge<unsigned long, boost::no_property>& e, const Skeleton& skeleton) { target(e, skeleton); },
-               "Returns the target vertex of the edge e.");
+        py::arg("skeleton"),
+        "Returns the number of edges in the skeleton.");
+  m.def("source",
+        [](const boost::list_edge<unsigned long, boost::no_property>& e,
+           const Skeleton& skeleton) { source(e, skeleton); },
+        py::arg("e"), py::arg("skeleton"),
+        "Returns the source vertex of the edge e.");
+  m.def("target",
+        [](const boost::list_edge<unsigned long, boost::no_property>& e,
+           const Skeleton& skeleton) { target(e, skeleton); },
+        py::arg("e"), py::arg("skeleton"),
+        "Returns the target vertex of the edge e.");
 
   using Visitor = boost_utils::Polyline_visitor<Skeleton>;
   py::class_<Visitor> pv(m, "Polyline_visitor");
@@ -239,19 +228,9 @@ void export_triangulated_surface_mesh_skeletonization(py::module_& m) {
         "Examples\n"
         "• Surface_mesh_skeletonization/simple_mcfskel_example.py.");
 
-  m.def("extract_mean_curvature_flow_skeleton", &CGAL::extract_mean_curvature_flow_skeleton<Tm>,
-        "extracts a medially centered curve skeleton for the triangle mesh tmesh. This function uses the class CGAL::Mean_curvature_flow_skeletonization with the default parameters. This function is provided only if Eigen 3.2 (or greater) is available and CGAL_EIGEN3_ENABLED is defined.\n"
-        "\n"
-        "Precondition\n"
-        "• tmesh is a triangle mesh without borders and having exactly one connected component. \n"
-        "• The specialization boost::property_map<TriangleMesh, CGAL::vertex_point_t>::type and get(vertex_point, tmesh) are defined. \n"
-        "• The value type of boost::property_map<TriangleMesh, CGAL::vertex_point_t>::type is a point type from a CGAL Kernel.\n"
-        "\n"
-        "Parameters\n"
-        "• tmesh:	input mesh\n"
-        "• skeleton:	graph that will contain the skeleton of tmesh. For each vertex descriptor vd of skeleton, the corresponding point and the set of input vertices that contracted to vd can be retrieved using skeleton[vd].point and skeleton[vd].vertices respectively.\n"
-        "\n"
-        "Examples\n"
-        "    Surface_mesh_skeletonization/segmentation_example.py, Surface_mesh_skeletonization/simple_mcfskel_example.py, and Surface_mesh_skeletonization/simple_mcfskel_sm_example.py.\n");
+  m.def("extract_mean_curvature_flow_skeleton",
+        &CGAL::extract_mean_curvature_flow_skeleton<Tm>,
+        py::arg("tmesh"), py::arg("skeleton"),
+        smsk_doc::extract_mean_curvature_flow_skeleton);
 
 }
