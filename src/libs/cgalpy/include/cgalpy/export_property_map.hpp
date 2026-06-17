05b2a965c44ba95b9e161865b8b71330dea8845c
Efi Fogel <efifogel@gmail.com>
2026-06-17 15:51:43 +0300

    Cleaned up (renamed Mesh with the more general term Graph"

--- src/libs/cgalpy/include/cgalpy/export_property_map.hpp
+++ src/libs/cgalpy/include/cgalpy/export_property_map.hpp
@@ -19,78 +19,79 @@ namespace py = nanobind;
 namespace internal {
 
 //!
-template <typename Mesh, typename Key, typename Value>
+template <typename Graph, typename Key, typename Value>
 auto export_property_map(py::module_& m, const std::string& name) {
-  using Mesh_property_map = typename Mesh::template Property_map<Key, Value>;
+  using Graph_property_map = typename Graph::template Property_map<Key, Value>;
 
-  m.def("get", [](const Mesh_property_map& p, const Key& t) { return get(p, t); },
+  m.def("get", [](const Graph_property_map& p, const Key& t) { return get(p, t); },
         py::arg("property_map"), py::arg("key"),
-        "Returns the value associated with a key in a mesh property map.");
+        "Returns the value associated with a key in a graph property map.");
 
   // return for extending with .def()
-  return py::class_<Mesh_property_map>(m, name.c_str(),
-                                      "Surface_mesh property map.")
+  return py::class_<Graph_property_map>(m, name.c_str(), "Graph property map.")
     .def(py::init<>(),
-         "Constructs an empty Surface_mesh property map.")
-    .def("__getitem__", [](Mesh_property_map& self, Key key) -> Value { return self[key]; },
+         "Constructs an empty graph property map.")
+    .def("__getitem__", [](Graph_property_map& self, Key key) -> Value { return self[key]; },
          py::arg("key"),
          "Returns the value associated with a key.")
-    .def("__setitem__", [](Mesh_property_map& self, Key key, Value value) { self[key] = value; },
+    .def("__setitem__", [](Graph_property_map& self, Key key, Value value) { self[key] = value; },
          py::arg("key"), py::arg("value"),
          "Sets the value associated with a key.")
-    .def("__iter__", [](Mesh_property_map& self) {
-      return py::make_iterator(py::type<typename Mesh_property_map::iterator>(),
-                               "Iterator", self.begin(), self.end(), py::keep_alive<0, 1>());
-    })
-    .def("transfer", [](Mesh_property_map& self, const Mesh_property_map& other) { return self.transfer(other); },
+    .def("__iter__",
+         [](Graph_property_map& self) {
+           return py::make_iterator(py::type<typename Graph_property_map::iterator>(),
+                                    "Iterator", self.begin(), self.end(), py::keep_alive<0, 1>());
+         })
+    .def("transfer", [](Graph_property_map& self, const Graph_property_map& other) { return self.transfer(other); },
          py::arg("other"),
          "Transfers values from another property map.")
-    .def("transfer", [](Mesh_property_map& self, const Mesh_property_map& other, std::size_t from, std::size_t to)
+    .def("transfer", [](Graph_property_map& self, const Graph_property_map& other, std::size_t from, std::size_t to)
     { return self.transfer(other, from, to); },
          py::arg("other"), py::arg("from"), py::arg("to"),
          "Transfers values from another property map in an index range.")
-    .def("reset", &Mesh_property_map::reset,
+    .def("reset", &Graph_property_map::reset,
          "Resets the property map.")
     ;
 }
 
 //!
-template <typename Mesh, typename Key>
+template <typename Graph, typename Key>
 auto export_property_map_bool(py::module_& m, const std::string& name) {
-  using Mesh_property_map = typename Mesh::template Property_map<Key, bool>;
+  using Graph_property_map = typename Graph::template Property_map<Key, bool>;
 
-  m.def("get", [](const Mesh_property_map& p, const Key& t) { return py::bool_(get(p, t)); },
+  m.def("get", [](const Graph_property_map& p, const Key& t) { return py::bool_(get(p, t)); },
         py::arg("property_map"), py::arg("key"),
-        "Returns the boolean value associated with a key in a mesh property map.");
+        "Returns the boolean value associated with a key in a graph property map.");
 
   // return for extending with .def()
-  return py::class_<Mesh_property_map>(m, name.c_str(),
-                                      "Boolean Surface_mesh property map.")
+  return py::class_<Graph_property_map>(m, name.c_str(), "Boolean graph property map.")
     .def(py::init<>(),
-         "Constructs an empty boolean Surface_mesh property map.")
-    .def("__getitem__", [](Mesh_property_map& self, Key key) -> bool { return self[key]; },
+         "Constructs an empty boolean graph property map.")
+    .def("__getitem__", [](Graph_property_map& self, Key key) -> bool { return self[key]; },
          py::arg("key"),
          "Returns the boolean value associated with a key.")
-    .def("__setitem__", [](Mesh_property_map& self, Key key, bool value) { self[key] = value; },
+    .def("__setitem__", [](Graph_property_map& self, Key key, bool value) { self[key] = value; },
          py::arg("key"), py::arg("value"),
          "Sets the boolean value associated with a key.")
-    // CGAL Surface_mesh<bool> property maps are backed by std::vector<bool>.
-    // Their iterator dereferences to a proxy reference, which nanobind cannot
-    // expose safely. Force the iterator value type to bool so Python receives
-    // ordinary bool values instead of std::vector<bool> proxy objects.
-    .def("__iter__", [](Mesh_property_map& self) {
-      using Iterator = typename Mesh_property_map::iterator;
-      return py::make_iterator<py::rv_policy::copy, Iterator, Iterator, bool>(
-        py::type<Iterator>(), "Iterator", self.begin(), self.end());
-    })
-    .def("transfer", [](Mesh_property_map& self, const Mesh_property_map& other) { return self.transfer(other); },
+    /* Boolean property maps are backed by `std::vector<bool>`. Their iterator
+     * dereferences to a proxy reference, which nanobind cannot expose safely.
+     * Force the iterator value type to `bool` so Python receives ordinary
+     * `bool` values instead of `std::vector<bool>` proxy objects.
+     */
+    .def("__iter__",
+         [](Graph_property_map& self) {
+           using Iterator = typename Graph_property_map::iterator;
+           return py::make_iterator<py::rv_policy::copy, Iterator, Iterator, bool>
+             (py::type<Iterator>(), "Iterator", self.begin(), self.end());
+         })
+    .def("transfer", [](Graph_property_map& self, const Graph_property_map& other) { return self.transfer(other); },
          py::arg("other"),
          "Transfers values from another boolean property map.")
-    .def("transfer", [](Mesh_property_map& self, const Mesh_property_map& other, std::size_t from, std::size_t to)
+    .def("transfer", [](Graph_property_map& self, const Graph_property_map& other, std::size_t from, std::size_t to)
     { return self.transfer(other, from, to); },
          py::arg("other"), py::arg("from"), py::arg("to"),
          "Transfers values from another boolean property map in an index range.")
-    .def("reset", &Mesh_property_map::reset,
+    .def("reset", &Graph_property_map::reset,
          "Resets the boolean property map.")
     ;
 }
