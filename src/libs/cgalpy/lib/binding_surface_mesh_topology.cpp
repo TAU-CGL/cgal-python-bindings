6b93845f8c397ab0f4a463060ade4d55320e5cdd
UtkarsHMer05 <utkarshkhajuria7@gmail.com>
2026-07-10 00:10:28 +0530

    Expose Surface Mesh topology path APIs

--- src/libs/cgalpy/lib/binding_surface_mesh_topology.cpp
+++ src/libs/cgalpy/lib/binding_surface_mesh_topology.cpp
@@ -10,6 +10,8 @@
 
 #include <nanobind/nanobind.h>
 #include <nanobind/stl/pair.h>
+#include <nanobind/stl/string.h>
+#include <nanobind/stl/vector.h>
 
 #include <CGAL/Curves_on_surface_topology.h>
 #include <CGAL/Path_on_surface.h>
@@ -66,6 +68,162 @@ std::pair<bool, bool> open_path_homotopy(const Surface_mesh_3& sm) {
 void export_surface_mesh_topology(py::module_& m) {
   py::class_<Smt::Polygonal_schema_min_items>(m, "Polygonal_schema_min_items")
     .def(py::init<>());
+
+  py::class_<Path_on_surface>(m, "Path_on_surface")
+    .def(py::init<const Surface_mesh_3&>(),
+         py::arg("mesh"),
+         py::keep_alive<1, 2>(),
+         "Creates an empty path on the given surface mesh.")
+    .def(py::init<const Path_on_surface&>(),
+         py::arg("other"),
+         "Copy constructor.")
+    .def("is_empty",
+         &Path_on_surface::is_empty,
+         "Returns true iff the path is empty.")
+    .def("length",
+         &Path_on_surface::length,
+         "Returns the number of darts in the path.")
+    .def("is_closed",
+         &Path_on_surface::is_closed,
+         "Returns true iff the path is closed.")
+    .def("clear",
+         &Path_on_surface::clear,
+         "Clears the path.")
+    .def("push_back_by_index",
+         [](Path_on_surface& path,
+            std::size_t index,
+            bool flip,
+            bool update_isclosed) {
+           path.push_back_by_index(index, flip, update_isclosed);
+         },
+         py::arg("index"),
+         py::arg("flip") = false,
+         py::arg("update_isclosed") = true,
+         "Adds the dart with the given index at the end of the path.")
+    .def("push_back_by_indices",
+         [](Path_on_surface& path,
+            const std::vector<std::size_t>& indices,
+            bool update_isclosed) {
+           for (std::size_t index : indices) {
+             path.push_back_by_index(index, false, update_isclosed);
+           }
+         },
+         py::arg("indices"),
+         py::arg("update_isclosed") = true,
+         "Adds the darts with the given indices at the end of the path.")
+    .def("extend_straight_positive",
+         [](Path_on_surface& path,
+            std::size_t nb,
+            bool update_isclosed) {
+           path.extend_straight_positive(nb, update_isclosed);
+         },
+         py::arg("nb") = 1,
+         py::arg("update_isclosed") = true,
+         "Extends the path straight in the positive direction.")
+    .def("extend_straight_negative",
+         [](Path_on_surface& path,
+            std::size_t nb,
+            bool update_isclosed) {
+           path.extend_straight_negative(nb, update_isclosed);
+         },
+         py::arg("nb") = 1,
+         py::arg("update_isclosed") = true,
+         "Extends the path straight in the negative direction.")
+    .def("extend_positive_turn",
+         [](Path_on_surface& path,
+            std::size_t nb,
+            bool update_isclosed) {
+           path.extend_positive_turn(nb, update_isclosed);
+         },
+         py::arg("nb") = 1,
+         py::arg("update_isclosed") = true,
+         "Extends the path with a positive turn.")
+    .def("extend_negative_turn",
+         [](Path_on_surface& path,
+            std::size_t nb,
+            bool update_isclosed) {
+           path.extend_negative_turn(nb, update_isclosed);
+         },
+         py::arg("nb") = 1,
+         py::arg("update_isclosed") = true,
+         "Extends the path with a negative turn.")
+    .def("simplify_flips",
+         &Path_on_surface::simplify_flips,
+         py::arg("show_flips_left") = false,
+         "Simplifies flips in the path when possible.")
+    .def("nb_flips",
+         &Path_on_surface::nb_flips,
+         "Returns the number of flips in the path.")
+    .def("cut",
+         &Path_on_surface::cut,
+         py::arg("n"),
+         py::arg("update_isclosed") = true,
+         "Cuts the path to keep only the first n darts.")
+    .def("initialize_random_starting_dart",
+         [](Path_on_surface& path,
+            bool update_isclosed) {
+           return path.initialize_random_starting_dart(update_isclosed);
+         },
+         py::arg("update_isclosed") = true,
+         "Initializes this path to a random starting dart.");
+
+  py::class_<Curves_on_surface_topology>(m, "Curves_on_surface_topology")
+    .def(py::init<const Surface_mesh_3&, bool>(),
+         py::arg("mesh"),
+         py::arg("display_time") = false,
+         py::keep_alive<1, 2>(),
+         "Creates a topology helper for curves on a surface mesh.")
+    .def("is_minimal_quadrangulation_computed",
+         &Curves_on_surface_topology::is_minimal_quadrangulation_computed,
+         "Returns true iff the minimal quadrangulation is computed.")
+    .def("compute_minimal_quadrangulation",
+         &Curves_on_surface_topology::compute_minimal_quadrangulation,
+         py::arg("display_time") = true,
+         "Computes the minimal quadrangulation if needed.")
+    .def("is_contractible",
+         [](const Curves_on_surface_topology& cst,
+            const Path_on_surface& path,
+            bool display_time) {
+           return cst.is_contractible(path, display_time);
+         },
+         py::arg("path"),
+         py::arg("display_time") = false,
+         "Returns true iff the path is contractible.")
+    .def("are_freely_homotopic",
+         [](const Curves_on_surface_topology& cst,
+            const Path_on_surface& path_1,
+            const Path_on_surface& path_2,
+            bool display_time) {
+           return cst.are_freely_homotopic(path_1, path_2, display_time);
+         },
+         py::arg("path_1"),
+         py::arg("path_2"),
+         py::arg("display_time") = false,
+         "Returns true iff the two paths are freely homotopic.")
+    .def("are_base_point_homotopic",
+         [](const Curves_on_surface_topology& cst,
+            const Path_on_surface& path_1,
+            const Path_on_surface& path_2,
+            bool display_time) {
+           return cst.are_base_point_homotopic(path_1, path_2, display_time);
+         },
+         py::arg("path_1"),
+         py::arg("path_2"),
+         py::arg("display_time") = false,
+         "Returns true iff the two paths are base point homotopic.")
+    .def("is_homotopic_to_simple_cycle",
+         [](const Curves_on_surface_topology& cst,
+            const Path_on_surface& path,
+            bool display_time) {
+           return cst.is_homotopic_to_simple_cycle(path, display_time);
+         },
+         py::arg("path"),
+         py::arg("display_time") = false,
+         "Returns true iff the path is homotopic to a simple cycle.")
+    .def("set_verbose",
+         &Curves_on_surface_topology::set_verbose,
+         py::arg("is_verbose"),
+         "Sets whether warning messages are displayed.");
 
   m.def("open_path_homotopy",
         &open_path_homotopy,
--- src/python_scripts/cgalpy_examples/Surface_mesh_topology/open_path_homotopy.py
+++ src/python_scripts/cgalpy_examples/Surface_mesh_topology/open_path_homotopy.py
@@ -17,10 +17,44 @@ filename = sys.argv[i] if len(sys.argv) > i else CGALPY.data_file_path("meshes/d
 
 mesh = Sm.read_polygon_mesh(filename)
 
-p1_p2, p2_p3 = Smt.open_path_homotopy(mesh)
+def create_path_1(path):
+  path.push_back_by_index(88)
+  for _ in range(3):
+    path.extend_positive_turn(2)
+
+def create_path_2(path):
+  path.push_back_by_index(300)
+  for _ in range(3):
+    path.extend_negative_turn(2)
+
+def create_path_3(path):
+  path.push_back_by_index(87)
+  path.extend_positive_turn(1)
+  for _ in range(3):
+    path.extend_positive_turn(2)
+  path.extend_positive_turn(1)
+
+cst = Smt.Curves_on_surface_topology(mesh)
+p1 = Smt.Path_on_surface(mesh)
+p2 = Smt.Path_on_surface(mesh)
+p3 = Smt.Path_on_surface(mesh)
+
+create_path_1(p1)
+create_path_2(p2)
+create_path_3(p3)
+
+p1_p2 = cst.are_base_point_homotopic(p1, p2)
+p2_p3 = cst.are_base_point_homotopic(p2, p3)
+
+helper_p1_p2, helper_p2_p3 = Smt.open_path_homotopy(mesh)
+
+assert p1_p2 == helper_p1_p2
+assert p2_p3 == helper_p2_p3
+assert p1.length() == 4
+assert p2.length() == 4
+assert p3.length() == 6
+assert isinstance(p1_p2, bool)
+assert isinstance(p2_p3, bool)
 
 print("Path p1 (pink)", "IS" if p1_p2 else "IS NOT", "base point homotopic with path p2 (green).")
 print("Path p2 (green)", "IS" if p2_p3 else "IS NOT", "base point homotopic with path p3 (orange).")
-
-assert isinstance(p1_p2, bool)
-assert isinstance(p2_p3, bool)
